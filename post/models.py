e7cc0fcdc6d8d0451c55eacd14d0dbdb380ce56f
Sana Javed <sanajaved7@gmail.com>
2015-12-17 12:52:39 -0500

    Created through table to connect Post and Subprogram models with a many to many relationship in Wagtail

--- post/models.py
+++ post/models.py
@@ -7,10 +7,16 @@ from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel, Inl
 from wagtail.wagtailsearch import index
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 from modelcluster.fields import ParentalKey
-from programs.models import Program
+from programs.models import Program, Subprogram
+
 
 
 class PostProgramRelationship(models.Model):
+    """
+    Through model that maps the many to many relationship 
+    between Post and Programs
+    """
+    
     program = models.ForeignKey(Program, related_name="+")
     post = ParentalKey('Post', related_name='programs')
 
@@ -18,10 +24,26 @@ class PostProgramRelationship(models.Model):
         FieldPanel('program'),
     ]
 
+class PostSubprogramRelationship(models.Model):
+    """
+    Through model that maps the many to many relationship 
+    between Post and Subprograms
+    """
+    subprogram = models.ForeignKey(Subprogram, related_name="+")
+    post = ParentalKey('Post', related_name='subprograms')
 
-#Abstract Post class that inherits from Page and provides a model template for other content type models
+    panels = [
+        FieldPanel('subprogram'),
+    ]
+
+
+
 class Post(Page):
-    """Abstract class for pages."""
+    """
+    Abstract Post class that inherits from Page 
+    and provides a model template for other content 
+    type models
+    """
 
     date = models.DateField("Post date")
     body = StreamField([
@@ -33,10 +55,13 @@ class Post(Page):
 
     parent_programs = models.ManyToManyField(Program, through=PostProgramRelationship, blank=True)
 
+    post_subprogram = models.ManyToManyField(Subprogram, through=PostSubprogramRelationship, blank=True)
+
     content_panels = Page.content_panels + [
         FieldPanel('date'),
         StreamFieldPanel('body'),
         InlinePanel('programs', label=("Programs")),
+        InlinePanel('subprograms', label=("Subprograms")),
     ]
 
     is_creatable = False
@@ -45,25 +70,54 @@ class Post(Page):
 
 
 class Book(Post):
-    """Book page"""
+    """
+    Book class that inherits from the abstract Post model
+    and creates pages for Books. 
+    """
     pass
 
 
 class Article(Post):
+    """
+    Article class that inherits from the abstract Post 
+    model and creates pages for Articles. 
+    """
     pass
 
 
 class Event(Post):
+    """
+    Event class that inherits from the abstract Post 
+    model and creates pages for Events. 
+    """
     pass
 
 
 class Podcasts(Post):
+    """
+    Podcast class that inherits from the abstract Post 
+    model and creates pages for Podcasts. 
+    """
     pass
 
 
 class PolicyPaper(Post):
+    """
+    Policy paper class that inherits from the abstract 
+    Post model and creates pages for Policy papers. 
+    """
     pass
 
 
 class About(Post):
+    """
+    About class that inherits from the abstract 
+    Post model and creates About Us pages. 
+    """
     pass
+
+class Blog(Post):
+    """
+    Blog class that inherits from the abstract 
+    Post model and creates pages for blog posts. 
+    """
