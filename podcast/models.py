2edecb42775336e856e24cfa0c520c8ede1ab8b4
Sana Javed <sanajaved7@gmail.com>
2015-12-21 17:28:25 -0500

    Refactored code to separate out Podcast pages

---
+++ podcast/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from post.models import Post
+
+class Podcasts(Post):
+    """
+    Podcast class that inherits from the abstract Post
+    model and creates pages for Podcasts.
+    """
+    pass
