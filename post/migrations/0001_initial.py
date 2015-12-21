755f31c7394791c95daa3a30ec81667ca365ca87
Sana Javed <sanajaved7@gmail.com>
2015-12-21 17:31:04 -0500

    Refactored code to separate out different types of content from main Post model

--- post/migrations/0001_initial.py
+++ post/migrations/0001_initial.py
@@ -3,34 +3,26 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 import modelcluster.fields
+import wagtail.wagtailimages.blocks
 import wagtail.wagtailcore.fields
 import wagtail.wagtailcore.blocks
-import wagtail.wagtailimages.blocks
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('wagtailcore', '0020_add_index_on_page_first_published_at'),
+        ('programs', '0002_auto_20151221_2222'),
+        ('person', '0002_auto_20151221_2222'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='book_home_page',
-            fields=[
-                ('page_ptr', models.OneToOneField(serialize=False, parent_link=True, to='wagtailcore.Page', primary_key=True, auto_created=True)),
-            ],
-            options={
-                'abstract': False,
-            },
-            bases=('wagtailcore.page',),
-        ),
-        migrations.CreateModel(
             name='Post',
             fields=[
-                ('page_ptr', models.OneToOneField(serialize=False, parent_link=True, to='wagtailcore.Page', primary_key=True, auto_created=True)),
-                ('date', models.DateField(verbose_name='Post date')),
-                ('body', wagtail.wagtailcore.fields.StreamField((('heading', wagtail.wagtailcore.blocks.CharBlock(classname='full title')), ('paragraph', wagtail.wagtailcore.blocks.RichTextBlock()), ('html', wagtail.wagtailcore.blocks.RawHTMLBlock()), ('image', wagtail.wagtailimages.blocks.ImageChooserBlock())))),
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
+                ('date', models.DateField(verbose_name=b'Post date')),
+                ('body', wagtail.wagtailcore.fields.StreamField([(b'heading', wagtail.wagtailcore.blocks.CharBlock(classname=b'full title')), (b'paragraph', wagtail.wagtailcore.blocks.RichTextBlock()), (b'html', wagtail.wagtailcore.blocks.RawHTMLBlock()), (b'image', wagtail.wagtailimages.blocks.ImageChooserBlock())])),
             ],
             options={
                 'abstract': False,
@@ -40,84 +32,40 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='PostAuthorRelationship',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('author', models.ForeignKey(related_name='+', to='person.Person')),
+                ('post', modelcluster.fields.ParentalKey(related_name='authors', to='post.Post')),
             ],
         ),
         migrations.CreateModel(
             name='PostProgramRelationship',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('post', modelcluster.fields.ParentalKey(related_name='programs', to='post.Post')),
+                ('program', models.ForeignKey(related_name='+', to='programs.Program')),
             ],
         ),
         migrations.CreateModel(
             name='PostSubprogramRelationship',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('post', modelcluster.fields.ParentalKey(related_name='subprograms', to='post.Post')),
+                ('subprogram', models.ForeignKey(related_name='+', to='programs.Subprogram')),
             ],
-        ),
-        migrations.CreateModel(
-            name='Article',
-            fields=[
-                ('post_ptr', models.OneToOneField(serialize=False, parent_link=True, to='post.Post', primary_key=True, auto_created=True)),
-            ],
-            options={
-                'abstract': False,
-            },
-            bases=('post.post',),
-        ),
-        migrations.CreateModel(
-            name='Blog',
-            fields=[
-                ('post_ptr', models.OneToOneField(serialize=False, parent_link=True, to='post.Post', primary_key=True, auto_created=True)),
-            ],
-            options={
-                'abstract': False,
-            },
-            bases=('post.post',),
-        ),
-        migrations.CreateModel(
-            name='Book',
-            fields=[
-                ('post_ptr', models.OneToOneField(serialize=False, parent_link=True, to='post.Post', primary_key=True, auto_created=True)),
-            ],
-            options={
-                'abstract': False,
-            },
-            bases=('post.post',),
-        ),
-        migrations.CreateModel(
-            name='Event',
-            fields=[
-                ('post_ptr', models.OneToOneField(serialize=False, parent_link=True, to='post.Post', primary_key=True, auto_created=True)),
-            ],
-            options={
-                'abstract': False,
-            },
-            bases=('post.post',),
-        ),
-        migrations.CreateModel(
-            name='Podcasts',
-            fields=[
-                ('post_ptr', models.OneToOneField(serialize=False, parent_link=True, to='post.Post', primary_key=True, auto_created=True)),
-            ],
-            options={
-                'abstract': False,
-            },
-            bases=('post.post',),
-        ),
-        migrations.CreateModel(
-            name='PolicyPaper',
-            fields=[
-                ('post_ptr', models.OneToOneField(serialize=False, parent_link=True, to='post.Post', primary_key=True, auto_created=True)),
-            ],
-            options={
-                'abstract': False,
-            },
-            bases=('post.post',),
         ),
         migrations.AddField(
-            model_name='postsubprogramrelationship',
-            name='post',
-            field=modelcluster.fields.ParentalKey(to='post.Post', related_name='subprograms'),
+            model_name='post',
+            name='parent_programs',
+            field=models.ManyToManyField(to='programs.Program', through='post.PostProgramRelationship', blank=True),
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='post_author',
+            field=models.ManyToManyField(to='person.Person', through='post.PostAuthorRelationship', blank=True),
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='post_subprogram',
+            field=models.ManyToManyField(to='programs.Subprogram', through='post.PostSubprogramRelationship', blank=True),
         ),
     ]
