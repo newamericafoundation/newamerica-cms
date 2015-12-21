d38a8360ea4ac4dd795a856ee8bfdfd0797a4efb
Sana Javed <sanajaved7@gmail.com>
2015-12-21 17:27:07 -0500

    Refactored code to separate out article pages

---
+++ article/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from post.models import Post
+
+from wagtail.wagtailcore.models import Page
+
+
+class Article(Post):
+    """
+    Article class that inherits from the abstract Post
+    model and creates pages for Articles.
+    """
+    pass
+
+
+class ArticleHomePage(Page):
+    parent_page_types = ['home.HomePage',]
+    subpage_types = []
+
+    def get_context(self, request):
+        context = super(ArticleHomePage, self).get_context(request)
+
+        context['articles'] = Article.objects.all()
+        return context
+
+    class Meta:
+        verbose_name = "Homepage for all Articles"
+
+
