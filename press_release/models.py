e8e9e9c7dafb90032b1c65e50db685920f5df055
Sana Javed <sanajaved7@gmail.com>
2015-12-21 17:29:01 -0500

    Refactored code to separate out Press Release pages

---
+++ press_release/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from post.models import Post
+
+
+class PressRelease(Post):
+    """
+    Press release class that inherits from the abstract
+    Post model and creates pages for press releases.
+    """
