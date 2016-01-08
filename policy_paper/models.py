c16d61206a31a8f342c86b8871f65219d53a8af0
Sana Javed <sanajaved7@gmail.com>
2016-01-08 15:20:11 -0500

    Minor edit in doc string

--- policy_paper/models.py
+++ policy_paper/models.py
@@ -13,7 +13,7 @@ from programs.models import Program
 class PolicyPaper(Post):
     """
     Policy paper class that inherits from the abstract
-    Post model and creates pages for Policy papers.
+    Post model and creates pages for Policy Papers.
     """
     parent_page_types = ['ProgramPolicyPapersPage']
     subpage_types = []
