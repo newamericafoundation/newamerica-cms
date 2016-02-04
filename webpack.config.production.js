b6c4eae72d5424447eb8567c23757f36478d536b
pickled-plugins <szerzo.peter@gmail.com>
2016-02-04 10:37:58 +0100

    added modernizr and fingerprinted javascript settings

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,54 +1,21 @@
 var path = require('path'),
 	webpack = require('webpack'),
-	CompressionPlugin = require('compression-webpack-plugin');
+	CompressionPlugin = require('compression-webpack-plugin'),
+	baseConfig = require('./webpack.config.base.js');
 
 module.exports = {
 
-	entry: './mysite/assets/js/mysite.js',
+	entry: baseConfig.entry,
+	output: baseConfig.output,
+	resolve: baseConfig.resolve,
+	module: baseConfig.module,
 
-	output: {
-		path: path.resolve('./mysite/static/js'),
-		publicPath: 'http://localhost:8000/',
-		filename: 'mysite.min.js',
-		sourceMapFilename: 'mysite.min.js.map'
-	},
-
-	resolve: {
-		modulesDirectories: [ 'node_modules' ]
-	},
-
-	module: {
-		loaders: [
-		
-			{
-				test: /(\.js)|(\.jsx)$/,
-				loader: 'babel-loader',
-				exclude: /(node_modules|bower_components)/,
-				query: {
-					presets: [ 'es2015', 'stage-0' ]
-				}
-			},
-
-			{
-				test: /\.scss$/,
-				loaders: [ 'style', 'css', 'sass' ]
-			},
-
-			{
-				test: /\.jade$/,
-				loaders: [ 'jade' ]
-			}
-
-		]
-	},
-
-	plugins: [
+	plugins: baseConfig.plugins.concat([
 		new webpack.optimize.UglifyJsPlugin({
 			mangle: {
 				except: [ '$super', '$', 'exports', 'require' ]
 			}
-		}),
-		new CompressionPlugin()
-	]
+		})
+	])
 
 }
