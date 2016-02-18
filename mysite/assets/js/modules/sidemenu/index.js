11b8b070080b19df0502321e987c7482112b0c60
Kirk Jackson <jacksonk@NAFiMac01-5.local>
2016-02-18 09:50:16 -0500

    fixed sidemenu resizing breakpoints

--- mysite/assets/js/modules/sidemenu/index.js
+++ mysite/assets/js/modules/sidemenu/index.js
@@ -2,15 +2,39 @@ import $ from 'jquery'
 
 import getJQueryObjects from './../../utilities/get_jquery_objects.js'
 
+var clicked = false;
+
 function addSidemenuInteractivity() {
 	$(".mobile-sidemenu__toggle").on("click", function() {
+		clicked = !clicked;
 		console.log("clicked!");
 		$(".sidemenu").toggle();
 		$(".content-container").toggle();
 		$("footer").toggle();
 	})
-
-
 }
 
 $(addSidemenuInteractivity)
+
+var $window = $( window ); // so you have a "cached" reference 
+var breakpoint = 640;
+
+$window.resize ( function () {
+	console.log("calling resize");
+	
+		
+		if ($window.width() > breakpoint ) {
+			// if (clicked == true ) {
+				console.log("width is greater");
+		    	$(".sidemenu").css("display", "block");
+				$(".content-container").css("display", "block");
+				$("footer").css("display", "block");
+				clicked = false;
+			
+	  	} else {
+	  		if (!clicked) {
+	  			$(".sidemenu").css("display", "none");
+	  		}
+	  	} 
+	
+});
