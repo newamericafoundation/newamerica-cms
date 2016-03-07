7ebf4ceb2655a0144f5e3742cb4ff9b08a184b38
Sana Javed <sanajaved7@gmail.com>
2016-03-07 17:05:32 -0500

    Refactored code to dynamically build sidebar for each Program page and its children pages. Updated logic to support child pages of any depth level and of Page type

--- home/templatetags/side_menu.py
+++ home/templatetags/side_menu.py
@@ -2,8 +2,12 @@ from datetime import date
 from django import template
 from django.conf import settings
 
+from wagtail.wagtailcore.models import Page
+
 from programs.models import Program, AbstractProgram, Subprogram
+
 from event.models import AllEventsHomePage
+
 from home.models import Post, SimplePage
 
 register = template.Library()
@@ -24,21 +28,24 @@ def needs_sidebar(context):
 # Retrieves the top menu items - the immediate children of the parent page
 @register.inclusion_tag('tags/side_menu.html', takes_context=True)
 def side_menu(context, parent, calling_page=None):
-
-    if isinstance(context['self'], Program):
-        menu_program = context['self']
-    elif isinstance(context['self'], Subprogram):
-        program_name = context['self'].get_parent()
-        menu_program = Program.objects.get(title=program_name)
-    elif isinstance(context['self'], Post) or isinstance(context['self'], SimplePage):
-        parent = context['self'].get_parent().get_parent()
-        if parent.depth == 3:
-            menu_program = Program.objects.get(title=parent)
-        else:
-            program_name = parent.get_parent()
-            menu_program = Program.objects.get(title=program_name)
    
-    def build_menu(menu_program):
+    def build_menu():
+        # if page_depth == 3:
+        #     menu_program = context['self']
+        # elif page_depth == 4:
+        #     program_name = context['self'].get_parent()
+        #     menu_program = Program.objects.get(title=program_name)
+        # elif page_depth == 5: 
+        #     program_name = context['self'].get_parent().get_parent()
+        #     menu_program = Program.objects.get(title=program_name)
+        page_depth = context['self'].depth
+        
+        if page_depth == 3:
+            menu_program = context['self']
+        elif page_depth > 3:
+            program_name = context['self'].get_ancestors()[2]
+            menu_program = Program.objects.get(title=program_name)
+
         context['url'] = menu_program.url
         context['logo'] = menu_program.title
         context['initiative_pages'] = menu_program.sidebar_menu_initiatives_and_projects_pages
@@ -47,5 +54,6 @@ def side_menu(context, parent, calling_page=None):
 
         return context
     
-    build_menu(menu_program)
+    build_menu()
+
     return context
