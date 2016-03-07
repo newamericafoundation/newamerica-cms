3d2430a44bdda78cd26fcfb75973d68ec729702c
Sana Javed <sanajaved7@gmail.com>
2016-03-07 15:56:29 -0500

    Starting writing the logic to dynamically generate the sidebar menu for Program content

--- home/templatetags/side_menu.py
+++ home/templatetags/side_menu.py
@@ -2,9 +2,9 @@ from datetime import date
 from django import template
 from django.conf import settings
 
-from programs.models import Program, AbstractProgram
+from programs.models import Program, AbstractProgram, Subprogram
 from event.models import AllEventsHomePage
-from home.models import Post
+from home.models import Post, SimplePage
 
 register = template.Library()
 
@@ -24,33 +24,28 @@ def needs_sidebar(context):
 # Retrieves the top menu items - the immediate children of the parent page
 @register.inclusion_tag('tags/side_menu.html', takes_context=True)
 def side_menu(context, parent, calling_page=None):
-    # # programs = Program.objects.in_menu().order_by("title").exclude(location=True)
-    # programs = []
-    # location_programs = []
-    # all_programs = Program.objects.in_menu().order_by("title")
-    # for program in all_programs:
-    #     if program.location == True:
-    #         location_programs.append(program)
-    #     else:
-    #         programs.append(program)
-    # return {
-    #     'calling_page': calling_page,
-    #     # 'programs': programs,
-    #     # 'location_programs': location_programs,
-    #     # required by the pageurl tag that we want to use within this template
-    #     'request': context['request'],
-    # }
-    return {}
 
+    if isinstance(context['self'], Program):
+        menu_program = context['self']
+    elif isinstance(context['self'], Subprogram):
+        program_name = context['self'].get_parent()
+        menu_program = Program.objects.get(title=program_name)
+    elif isinstance(context['self'], Post) or isinstance(context['self'], SimplePage):
+        parent = context['self'].get_parent().get_parent()
+        if parent.depth == 3:
+            menu_program = Program.objects.get(title=parent)
+        else:
+            program_name = parent.get_parent()
+            menu_program = Program.objects.get(title=program_name)
+   
+    def build_menu(menu_program):
+        context['url'] = menu_program.url
+        context['logo'] = menu_program.title
+        context['initiative_pages'] = menu_program.sidebar_menu_initiatives_and_projects_pages
+        context['work_pages'] = menu_program.sidebar_menu_our_work_pages
+        context['about_pages'] = menu_program.sidebar_menu_about_us_pages
 
-# Retrieves the children of the top menu items for the drop downs
-# @register.inclusion_tag('tags/top_menu_children.html', takes_context=True)
-# def top_menu_children(context, parent):
-#     subprograms = parent.subprogram_set.all()
-
-#     return {
-#         'parent': parent,
-#         'subprograms': subprograms,
-#         # required by the pageurl tag that we want to use within this template
-#         'request': context['request'],
-#     }
+        return context
+    
+    build_menu(menu_program)
+    return context
