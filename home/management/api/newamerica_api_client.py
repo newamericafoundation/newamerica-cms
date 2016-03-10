76ff5c63d02a5a7bb7450e72e83ac4ede17da634
Sana <sanajaved7@gmail.com>
2016-03-09 23:27:45 -0500

    Created a class to pull data from the API

---
+++ home/management/api/newamerica_api_client.py
@@ -0,0 +1,33 @@
+import requests
+
+import os
+
+
+class NAClient:
+	def __init__(self, endpoint):
+		self.login_url = 'https://admin.newamerica.org/login/'
+		self.api_url = 'https://admin.newamerica.org/api/' + endpoint
+		self.login_data = {
+			'username': os.getenv('API_EMAIL'), 
+			'password': os.getenv('API_PASSW')
+		}
+		self.client = requests.Session()
+		self.authenticate()
+
+	def authenticate(self):
+		headers = {'Referer': self.login_url}
+		csrf = self.client.get(self.login_url).cookies['csrftoken']
+		self.login_data['csrfmiddlewaretoken'] = csrf
+		login_attempt = self.client.post(self.login_url, data=self.login_data, headers=headers)
+		assert login_attempt.status_code == 200
+
+	def get_data(self):
+		endpoint = self.api_url
+
+		while endpoint:
+			response = self.client.get(endpoint).json()
+			endpoint = response.get('next')
+			yield response
+
+
+
