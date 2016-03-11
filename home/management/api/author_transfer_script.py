05932e289c8b779d7ecfe51f4c99c61b55481854
Sana Javed <sanajaved7@gmail.com>
2016-03-11 11:49:29 -0500

    Removing unused imports, PEP8 style changes

--- home/management/api/author_transfer_script.py
+++ home/management/api/author_transfer_script.py
@@ -1,4 +1,3 @@
-import json 
 import sys
 
 import csv
@@ -8,7 +7,7 @@ import urllib
 from django.utils.text import slugify
 from django.core.files.images import ImageFile
 
-from wagtail.wagtailimages.models import Image 
+from wagtail.wagtailimages.models import Image
 
 from person.models import OurPeoplePage, Person
 
@@ -17,95 +16,109 @@ from .newamerica_api_client import NAClient
 our_people_page = OurPeoplePage.objects.first()
 
 if sys.version_info[0] < 3:
-	reload(sys)  # noqa
-	sys.setdefaultencoding('utf-8')
+    reload(sys)  # noqa
+    sys.setdefaultencoding('utf-8')
 
-
-def load_transformation():
-	with open('home/management/api/author_cleanup.json', "r") as stream:
-		return json.load(stream)
 
 def load_users_mapping():
-	csv_data = {}
-	with open('home/management/api/authors.csv', "r") as csvfile:
-		 csv_reader = csv.reader(csvfile)
-		 for row in csv_reader:
-		 	csv_data[str(row[0])] = {'first_name': row[2], 'last_name': row[3], 'duplicate': row[4], 'merged_with': row[5]}
-	return csv_data
+    csv_data = {}
+    with open('home/management/api/authors.csv', "r") as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for row in csv_reader:
+            csv_data[str(row[0])] = {
+                'first_name': row[2],
+                'last_name': row[3],
+                'duplicate': row[4],
+                'merged_with': row[5]
+            }
+    return csv_data
 
 
 def users_data_stream():
-	users = NAClient('users')
-	for user_set in users.get_data():
-		for user in user_set['results']:
-			yield user
-		
+    users = NAClient('users')
+    for user_set in users.get_data():
+        for user in user_set['results']:
+            yield user
+
 
 def download_image(url, image_filename):
-	if url:
-		image_location = os.path.join('home/management/api/images', image_filename)
-		urllib.urlretrieve(url, image_location)
-		image = Image(title=image_filename, file=ImageFile(open(image_location), name=image_filename))
-		image.save()
-		return image
+    if url:
+        image_location = os.path.join(
+            'home/management/api/images',
+            image_filename
+        )
+        urllib.urlretrieve(url, image_location)
+        image = Image(
+            title=image_filename,
+            file=ImageFile(open(image_location), name=image_filename)
+        )
+        image.save()
+        return image
+
 
 def get_role_info(old_role):
 
-	new_roles = {
-		"staff": 'Staff', 
-		"board": 'Board Member', 
-		"fellow": 'New America Fellow', 
-		"alumni": 'External Author/Former Staff',
-		"programfellow": 'Program Fellow',
-	}
-	expert_roles = ['Board Member', 'New America Fellow', 'Program Fellow']
-	role_info = {}
-	
-	if not old_role:
-		role_info['role_title'] = 'External Author/Former Staff'
-		role_info['expert'] = False
-	else:
-		role_info['role_title'] = new_roles.get(old_role[0], 'External Author/Former Staff')
-		if role_info['role_title'] in expert_roles:
-			role_info['expert'] = True
-		else:
-			role_info['expert'] = False
+    new_roles = {
+        "staff": 'Staff',
+        "board": 'Board Member',
+        "fellow": 'New America Fellow',
+        "alumni": 'External Author/Former Staff',
+        "programfellow": 'Program Fellow',
+    }
+    expert_roles = ['Board Member', 'New America Fellow', 'Program Fellow']
+    role_info = {}
 
-	return role_info
+    if not old_role:
+        role_info['role_title'] = 'External Author/Former Staff'
+        role_info['expert'] = False
+    else:
+        role_info['role_title'] = new_roles.get(
+            old_role[0],
+            'External Author/Former Staff'
+        )
+        if role_info['role_title'] in expert_roles:
+            role_info['expert'] = True
+        else:
+            role_info['expert'] = False
+
+    return role_info
+
 
 def load_authors():
-	users_mapping = load_users_mapping()
-	for user_api in users_data_stream():
-		# Look for user in csv data
-		mapped_user = users_mapping[str(user_api['id'])]
-		if not mapped_user['duplicate']:
-			mapped_user_title = '{0} {1}'.format(mapped_user['first_name'], mapped_user['last_name'])
-			mapped_user_slug = slugify(mapped_user_title)
-			db_user = Person.objects.filter(slug=mapped_user_slug).first()
-		if not db_user and mapped_user_slug:
-			print(mapped_user_slug, mapped_user_title, mapped_user['first_name'], mapped_user['last_name'], user_api['email'])
-			role_info = get_role_info(user_api['roles'])
-			db_user = Person(
-				search_description='', 
- 				seo_title='', 
- 				show_in_menus=False,
- 				slug=mapped_user_slug,
-				title=mapped_user_title, 
-				first_name=mapped_user['first_name'], 
-				last_name=mapped_user['last_name'], 
-				position_at_new_america=user_api['title'],
-				role=role_info['role_title'],
-				email=user_api['email'],
-				expert=role_info['expert'],
-				depth=4,
-				profile_image=download_image(user_api['image'], mapped_user_slug + "_image.jpeg"),
-				short_bio=user_api['short_bio'],
-				long_bio=user_api['long_bio'],
-			)
-			our_people_page.add_child(instance=db_user)
-			db_user.save()
-			
+    users_mapping = load_users_mapping()
+    for user_api in users_data_stream():
+        mapped_user = users_mapping[str(user_api['id'])]
+        if not mapped_user['duplicate']:
+            mapped_user_title = '{0} {1}'.format(
+                mapped_user['first_name'],
+                mapped_user['last_name'])
+            mapped_user_slug = slugify(mapped_user_title)
+            db_user = Person.objects.filter(slug=mapped_user_slug).first()
+        if not db_user and mapped_user_slug:
+            role_info = get_role_info(user_api['roles'])
+            db_user = Person(
+                search_description='', 
+                seo_title='', 
+                show_in_menus=False,
+                slug=mapped_user_slug,
+                title=mapped_user_title, 
+                first_name=mapped_user['first_name'], 
+                last_name=mapped_user['last_name'], 
+                position_at_new_america=user_api['title'],
+                role=role_info['role_title'],
+                email=user_api['email'],
+                expert=role_info['expert'],
+                depth=4,
+                profile_image=download_image(
+                    user_api['image'],
+                    mapped_user_slug + "_image.jpeg"
+                ),
+                short_bio=user_api['short_bio'],
+                long_bio=user_api['long_bio'],
+            )
+            our_people_page.add_child(instance=db_user)
+            db_user.save()
 
 
 def run():
-	load_authors()
+    load_authors()
