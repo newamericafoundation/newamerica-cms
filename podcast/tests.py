7e2533c3900bc2d500033078d66f1e837ce74d24
Sana Javed <sanajaved7@gmail.com>
2016-03-14 16:59:03 -0400

    Additional tests for the Podcast model to ensure relationship between podcast and one program, podcasts and two parent programs, and tests to ensure when Podcast is deleted so is the relationship it has with parent programs in the PostProgramRelationship model

--- podcast/tests.py
+++ podcast/tests.py
@@ -30,7 +30,8 @@ class PodcastTests(WagtailPageTests):
             instance=Program(title='OTI', name='OTI', location=False, depth=3)
         )
         self.program_podcasts_page = self.program_page_1.add_child(
-            instance=ProgramPodcastsPage(title='OTI Podcasts')
+            instance=ProgramPodcastsPage(
+                title='OTI Podcasts')
         )
         self.podcast = self.program_podcasts_page.add_child(
             instance=Podcast(title='Podcast 1', date='2016-02-10')
@@ -74,4 +75,83 @@ class PodcastTests(WagtailPageTests):
         self.assertCanCreate(self.program_page_1, ProgramPodcastsPage, {
             'title': 'Our Program Podcasts',
             }
+        )
+
+    # Test relationship between podcast and one
+    # Program
+    def test_podcast_has_relationship_to_one_program(self):
+        podcast = Podcast.objects.first()
+        self.assertEqual(podcast.parent_programs.all()[0].title, 'OTI')
+
+    # Test you can create a podcast with two parent Programs
+    def test_podcast_has_relationship_to_two_parent_programs(self):
+        podcast = Podcast.objects.first()
+        second_program = Program.objects.create(
+            title='Education', name='Education', location=False, depth=3)
+        relationship, created = PostProgramRelationship.objects.get_or_create(
+            program=second_program, post=podcast)
+        relationship.save()
+        self.assertEqual(
+            podcast.parent_programs.filter(title='Education').first().title,
+            'Education'
+        )
+
+    # Test Podcast page with one parent Program can be deleted
+    def test_podcast_with_one_parent_program_can_be_deleted(self):
+        podcast = Podcast.objects.filter(title='Podcast 1').first()
+        podcast.delete()
+        self.assertEqual(Podcast.objects.filter(
+            title='Podcast 1').first(), None
+        )
+        self.assertNotIn(
+            podcast,
+            ProgramPodcastsPage.objects.filter(
+                title='OTI Podcasts').first().get_children()
+        )
+        self.assertEqual(
+            PostProgramRelationship.objects.filter(
+                post=podcast).first(), None
+        )
+        self.assertEqual(
+            PostProgramRelationship.objects.filter(
+                post=podcast,
+                program=self.program_page_1).first(),
+            None
+        )
+
+    # Test Podcast page with two parent Programs can be deleted
+    def test_podcast_with_two_parent_programs_can_be_deleted(self):
+        podcast = Podcast.objects.filter(
+            title='Podcast 1').first()
+        program_page_2 = Program.objects.create(
+            title='Education',
+            name='Education',
+            location='False',
+            depth=3
+        )
+        relationship, created = PostProgramRelationship.objects.get_or_create(
+            program=program_page_2,
+            post=podcast
+            )
+        if created:
+            relationship.save()
+        podcast.delete()
+        self.assertEqual(Podcast.objects.filter(
+            title='Podcast 1').first(), None
+        )
+        self.assertNotIn(
+            podcast,
+            ProgramPodcastsPage.objects.filter(
+                title='OTI Podcasts').first().get_children()
+        )
+        self.assertEqual(
+            PostProgramRelationship.objects.filter(
+                post=podcast,
+                program=self.program_page_1).first(),
+            None
+        )
+        self.assertEqual(
+            PostProgramRelationship.objects.filter(
+                post=podcast,
+                program=program_page_2).first(), None
         )
