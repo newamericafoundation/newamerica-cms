280f04c911a4e6c550fe2d4a52e19ea6a72b3e33
Sana Javed <sanajaved7@gmail.com>
2016-03-15 16:47:14 -0400

    Building out the post transfer script

--- home/management/api/post_transfer_script.py
+++ home/management/api/post_transfer_script.py
@@ -1,5 +1,54 @@
 from .newamerica_api_client import NAClient
 
+from article.models import Article
+
+from django.utils.text import slugify
+
+def get_programs(program_id):
+	str(program_id)
+	
+	old_programs = {
+		'15': 'Asset Building',
+		'7': 'Better Life Lab',
+		'19': 'Cybersecurity Initiative',
+		'13': 'Economic Growth',
+		'5': 'Education Policy',
+		'20': 'Family Centered Social Policy',
+		'1': 'Future of War',
+		'9': 'Fellows',
+		'2': 'Future Tense',
+		'22': 'Global Cybersecurity Norms',
+		'10': 'International Security',
+		'8': 'New America',
+		'24': 'New America CA',
+		'17': 'New America Live',
+		'18': 'New America NYC',
+		'16': 'Open Markets',
+		'3': 'Open Technology Institute',
+		'6': 'Political Reform',
+		'14': 'Post Secondary National Policy Institute',
+		'21': 'Profits & Purpose',
+		'23': 'Resilient Communities',
+		'25': 'Resource Security',
+		'12': 'The Weekly Wonk',
+		'4': 'Youthsave',
+	}
+
 def load_posts():
     for post in NAClient().get_posts():
-        print(post['programs'])
+        if post['type'] == "Article":
+        	article_slug = slugify(post['title'])
+        	new_article = Article.objects.filter(slug=article_slug).first()
+        	if not new_article:
+	        	new_article = Article(
+	        		search_description='',
+	        		seo_title='',
+	        		show_in_menus=False,
+	        		slug=article_slug,
+	        		title=post['title'],
+	        		date='2016-03-15',
+
+
+	        	)
+
+
