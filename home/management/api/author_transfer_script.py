8b9aa6633992f0c849467721b1520a4f0baa8dc8
Sana Javed <sanajaved7@gmail.com>
2016-03-18 16:41:34 -0400

    Updating imports in file to include the articles script

--- home/management/api/author_transfer_script.py
+++ home/management/api/author_transfer_script.py
@@ -13,7 +13,7 @@ from person.models import OurPeoplePage, Person
 
 from .newamerica_api_client import NAClient
 
-from .post_transfer_script import load_posts
+from .post_transfer_script import load_articles
 
 our_people_page = OurPeoplePage.objects.first()
 
@@ -149,5 +149,5 @@ def load_authors():
 
 
 def run():
-    load_authors()
-    # load_posts()
+    #load_authors()
+    load_articles()
