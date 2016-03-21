82df1b3a89801ef4552f79dd99db6fa7f4b1b76e
Sana Javed <sanajaved7@gmail.com>
2016-03-21 12:56:13 -0400

    More progress on moving posts over into new database. Still a work in progress but now are able to connect post to program and correct parent page type. Next is connecting authors

--- home/management/api/post_transfer_script.py
+++ home/management/api/post_transfer_script.py
@@ -1,12 +1,25 @@
+import sys
+
+import csv
+import os
+import urllib
+
+import datetime
+
+from wagtail.wagtailimages.models import Image
+
 from .newamerica_api_client import NAClient
 
 from article.models import Article, ProgramArticlesPage
 
 from django.utils.text import slugify
+from django.core.files.images import ImageFile
 
 from home.models import PostProgramRelationship
 
 from programs.models import Program
+
+import json
 
 mapped_programs = {
 		'15': 'Asset Building',
@@ -46,16 +59,19 @@ def get_program(programs, post):
 
 
 def get_post_date(original_date):
-	old_date_split = original_date.split("T")
-	new_date = old_date_split[0]
-	
+	if original_date:
+		old_date_split = original_date.split("T")
+		new_date = old_date_split[0]
+	else:
+		new_date = '2016-03-21'
+
 	return new_date
 
 
 def download_image(url, image_filename):
     if url:
         image_location = os.path.join(
-            'home/management/api/images',
+            'home/management/api/article_images',
             image_filename
         )
         urllib.urlretrieve(url, image_location)
@@ -67,18 +83,30 @@ def download_image(url, image_filename):
         return image
 
 
+def need_to_update_post(modified_date):
+	today = datetime.datetime.today()
+	one_month_ago = today - datetime.timedelta(days=30)
+	
+	formatted_revision_date = get_post_date(modified_date)
+	formatted_revision_date = datetime.datetime.strptime(formatted_revision_date , '%Y-%m-%d')
+	formatted_revision_date = formatted_revision_date.date()
+	print(formatted_revision_date)
+
+	if formatted_revision_date >= one_month_ago.date():
+		return True
+
 def load_articles():
     for post, program_id in NAClient().get_articles():
     	program_id = str(program_id)
-    	post_parent_program = Program.objects.get_or_create(title=mapped_programs[program_id])
+    	post_parent_program = Program.objects.get_or_create(title=mapped_programs[program_id])[0]
 
-    	parent_program_articles_homepage = first_parent_program.add_child(instance=)
-
+    	parent_program_articles_homepage = post_parent_program.get_children().type(ProgramArticlesPage).first()
 
     	article_slug = slugify(post['title'])
+
     	new_article = Article.objects.filter(slug=article_slug).first()
     	
-    	if not new_article:
+    	if not new_article and article_slug:
         	new_article = Article(
         		search_description='',
         		seo_title='',
@@ -89,5 +117,30 @@ def load_articles():
         		date=get_post_date(post['publish_at']),
         		body=json.dumps([{'type':'paragraph', 'value':post['content']}]),
         		story_exerpt=post['summary'],
-        		story_image=download_image(post['cover_image_url'], post['title'] = "_image.jpeg")
+        		story_image=download_image(
+        			post['cover_image_url'], 
+        			post['title'] + "_image.jpeg"
+        		)
         	)
+        	print("Adding new article: ")
+        	print(new_article)
+        	parent_program_articles_homepage.add_child(instance=new_article)
+        	new_article.save()
+        elif new_article and article_slug and need_to_update_post(post['modified']):
+        	new_article.search_description=''
+        	new_article.seo_title=''
+        	new_article.depth=4
+        	new_article.show_in_menus=False
+        	new_article.slug=article_slug
+        	new_article.title=post['title']
+        	new_article.date=get_post_date(post['publish_at'])
+        	new_article.body=json.dumps(
+        		[{'type':'paragraph', 'value':post['content']}]
+        	)
+        	new_article.story_image=download_image(
+        			post['cover_image_url'], 
+        			post['title'] + "_image.jpeg"
+        		)
+        	print("Updating existing article: ")
+        	print(new_article)
+        	new_article.save()
