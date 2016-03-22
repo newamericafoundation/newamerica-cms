ddb2fa4177f0b5c150ac77f69cdfc7432e5be0c6
Sana Javed <sanajaved7@gmail.com>
2016-03-22 15:23:16 -0400

    Script to paginate search results for content homepages across the site

---
+++ mysite/pagination.py
@@ -0,0 +1,14 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+def paginate_results(request, all_posts):
+    page = request.GET.get('page')
+    paginator = Paginator(all_posts, 10)
+    
+    try:
+        all_posts = paginator.page(page)
+    except PageNotAnInteger:
+        all_posts = paginator.page(1)
+    except EmptyPage:
+        all_posts = paginator.page(paginator.num_pages)
+
+    return all_posts
