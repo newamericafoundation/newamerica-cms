082289c4758ac42cac6aef573ff5d223f8975528
kjacks <kjack227@gmail.com>
2016-03-25 16:04:25 -0400

    added program logo to sidemenu

--- home/templatetags/side_menu.py
+++ home/templatetags/side_menu.py
@@ -18,7 +18,7 @@ def side_menu(context, parent, calling_page=None):
         menu_program = Program.objects.get(title=program_name)
     context['side_menu'] = {}
     context['side_menu']['url'] = menu_program.url
-    context['side_menu']['logo'] = menu_program.title
+    context['side_menu']['logo'] = menu_program.program_logo
     context['side_menu']['initiative_pages'] = menu_program.sidebar_menu_initiatives_and_projects_pages
     context['side_menu']['work_pages'] = menu_program.sidebar_menu_our_work_pages
     context['side_menu']['about_pages'] = menu_program.sidebar_menu_about_us_pages
