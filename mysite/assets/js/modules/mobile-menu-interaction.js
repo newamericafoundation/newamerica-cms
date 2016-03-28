fea5146e57d63856bf5edf53f04090c609b93364
kjacks <kjack227@gmail.com>
2016-03-28 18:19:31 -0400

    fixed mobile menu interactivity

--- mysite/assets/js/modules/mobile-menu-interaction.js
+++ mysite/assets/js/modules/mobile-menu-interaction.js
@@ -23,7 +23,7 @@ function addMenuResponsivity() {
 
 		// toggle header menu and ensure sidemenu hidden
 		$(".mobile-header").toggle();
-		$(".sidemenu").css("display", "none");
+		$(".sidemenu-container").css("display", "none");
 		sideMenuExpanded = false;
 
 		// if expanding header, hide content and footer, else show content and footer
@@ -43,17 +43,19 @@ function addMenuResponsivity() {
 		// toggle arrow for sidemenu, ensure arrow up for header
 		$(".mobile-nav__toggle__secondary").toggleClass("down");
 		$(".mobile-nav__toggle__main").removeClass("down");
-
-		$(".sidemenu").toggle();
+		
+		$(".mobile-sidemenu").css("display", "block");
 		$(".mobile-header").css("display", "none");
 		headerMenuExpanded = false;
 		// sidemenuExpanded = !sidemenuExpanded;
 
 		if (sideMenuExpanded) {
+			$(".sidemenu-container").css("display", "none");
 			$(".content-container").css("display", "table-cell");
 			$("footer").css("display", "block");
 			sideMenuExpanded = false;
 		} else {
+			$(".sidemenu-container").css("display", "table-cell");
 			$(".content-container").css("display", "none");
 			$("footer").css("display", "none");
 			sideMenuExpanded = true;
@@ -89,7 +91,9 @@ function WidthChange(mq) {
     console.log("width is greater");
 	$(".mobile-nav").css("display", "none");
 	$(".mobile-header").css("display", "none");
+	$(".mobile-sidemenu").css("display", "none");
 	$(".sidemenu").css("display", "block");
+	$(".sidemenu-container").css("display", "table-cell");
 	$(".content-container").css("display", "table-cell");
 	$("footer").css("display", "block");
 	sideMenuExpanded = false;
@@ -97,9 +101,10 @@ function WidthChange(mq) {
   } else {
     console.log("width is less");
 	$(".mobile-nav").css("display", "block");
+	$(".sidemenu").css("display", "none");
 
 	if (!sideMenuExpanded) {
-		$(".sidemenu").css("display", "none");
+		$(".sidemenu-container").css("display", "none");
 		$(".content-container").css("display", "block");
 	}
 	if (!headerMenuExpanded) {
