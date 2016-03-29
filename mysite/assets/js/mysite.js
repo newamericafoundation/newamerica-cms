3d3924aa2b6bc48da6a4eec3b443401a467aaf4a
kjacks <kjack227@gmail.com>
2016-03-29 10:54:34 -0400

    removed commented lines

--- mysite/assets/js/mysite.js
+++ mysite/assets/js/mysite.js
@@ -2,15 +2,11 @@ import './../scss/mysite.scss'
 
 import 'babel-polyfill'
 
-
 import './utilities/index.js'
 import './modules/index.js'
-// import jQuery, $ from 'jquery'
-import 'script!jquery'
-// import 'script!what-input'
-import 'script!foundation-sites/dist/foundation.js'
 
-// require('./../../../node_modules/foundation-sites/dist/foundation.js');
+import 'script!jquery'
+import 'script!foundation-sites/dist/foundation.js'
 
 $(document).ready(function() {
 	$(document).foundation();
