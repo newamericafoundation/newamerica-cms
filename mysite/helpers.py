285f3a8b728e8e84d04e08c365e9fe6dac97f334
Sana Javed <sanajaved7@gmail.com>
2016-03-28 21:53:23 -0400

    Creating a file for helper functions. First is to return content for a program content homepage

---
+++ mysite/helpers.py
@@ -0,0 +1,25 @@
+from programs.models import Program, Subprogram
+from mysite.pagination import paginate_results
+
+
+def get_posts_and_programs(self, request, page_type, content_model):
+    """
+    Function to return a list of content for a program or 
+    subprogram content homepage
+    """
+    context = super(page_type, self).get_context(request)
+    
+    if self.depth == 4:
+        program_title = self.get_ancestors()[2]
+        program = Program.objects.get(title=program_title)
+        all_posts = content_model.objects.filter(parent_programs=program)
+    else:
+        subprogram_title = self.get_ancestors()[3]
+        program = Subprogram.objects.get(title=subprogram_title)
+        all_posts = content_model.objects.filter(post_subprogram=program)
+    
+    context['all_posts'] = paginate_results(request, all_posts)
+
+    context['program'] = program
+    
+    return context
