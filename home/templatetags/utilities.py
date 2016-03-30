54c37a011d9544b059acdb96de50cef25bf79080
kjacks <kjack227@gmail.com>
2016-03-30 13:37:40 -0400

    added date time tag to content grid

--- home/templatetags/utilities.py
+++ home/templatetags/utilities.py
@@ -29,7 +29,7 @@ def pluralize(item):
 	else:
 		return ""
 
-def plural_label(num_items, label):
+def pluralize_label(num_items, label):
 	if num_items > 1:
 		return label + "s: "
 	else:
@@ -44,13 +44,13 @@ def generate_byline(type, authors):
 	if post_type == "event" or post_type == "press release":
 		return ""
 	elif post_type == "podcast":
-		ret_string += plural_label(num_authors, "Host")
+		ret_string += pluralize_label(num_authors, "Host")
 	elif post_type == "blog post":
 		ret_string += "By "
 	elif post_type == "In The News Piece":
 		ret_string += "In the News: "
 	else:
-		ret_string += plural_label(num_authors, "Author")
+		ret_string += pluralize_label(num_authors, "Author")
 
 	counter = 1
 	for author in authors:
@@ -60,3 +60,27 @@ def generate_byline(type, authors):
 
 	print ret_string
 	return mark_safe(ret_string)
+
+@register.simple_tag()
+def generate_dateline(post):
+	ret_string = ""
+	date_format = '%B %d, %Y'
+	time_format = '%-I:%M %p'
+
+	
+	if post.date:
+		ret_string += '<p class="date">'
+		ret_string += post.date.strftime(date_format)
+		if post.end_date:
+			ret_string += ' - ' + post.end_date.strftime(date_format)
+		ret_string += '</p>'
+
+	if post.start_time:
+		ret_string += '<p class="time">'
+		ret_string += post.start_time.strftime(time_format).lower()
+		if post.end_time:
+			ret_string += ' - ' + post.end_time.strftime(time_format).lower()
+		ret_string += '</p>'
+
+	return mark_safe(ret_string)
+
