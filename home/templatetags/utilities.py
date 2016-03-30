96be485242add70e30814ba191e6a30d515ffd26
kjacks <kjack227@gmail.com>
2016-03-30 12:57:37 -0400

    added generate byline tag to all content grids

--- home/templatetags/utilities.py
+++ home/templatetags/utilities.py
@@ -7,11 +7,11 @@ from django.utils.safestring import mark_safe
 register = template.Library()
 
 @register.simple_tag()
-def listify(i):
+def list_separator(i):
 	if i >= 2:
-		return ","
+		return ", "
 	elif i == 1:
-		return " and"
+		return " and "
 	else:
 		return ""
 
@@ -35,40 +35,28 @@ def plural(num_items, label):
 	else:
 		return label + ""
 
-def listifize(reverse_counter):
-	if reverse_counter >= 2:
-		return ", "
-	elif reverse_counter == 1:
-		return " and "
-	else:
-		return ""
-
 @register.simple_tag()
 def generate_byline(type, authors):
 	post_type = str(type)
 	num_authors = len(authors)
-	print num_authors
 	ret_string = ""
 	
 	if post_type == "event" or post_type == "press release":
 		return ""
 	elif post_type == "podcast":
 		ret_string += plural(num_authors, "Host")
-	elif post_type == "blog":
+	elif post_type == "blog post":
 		ret_string += "By "
 	elif post_type == "In The News Piece":
 		ret_string += "In the News: "
 	else:
 		ret_string += plural(num_authors, "Author")
 
-	print authors[1].url
-
 	counter = 1
 	for author in authors:
 		ret_string += '<a href="' + author.url + '">' + author.first_name + ' ' + author.last_name + '</a>'
-		ret_string += listifize(num_authors - counter)
+		ret_string += list_separator(num_authors - counter)
 		counter += 1
 
-
 	print ret_string
-	return mark_safe('<p>' + ret_string + '</p>')
+	return mark_safe(ret_string)
