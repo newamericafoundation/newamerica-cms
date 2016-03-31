2ebe758081fe5e3860caccbdf5a6233a2d783396
kjacks <kjack227@gmail.com>
2016-03-31 14:05:52 -0400

    finished formatting for pagination

--- mysite/assets/js/modules/pagination.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery'
-
-$(document).ready(function() {
-	$(".pagination__form").submit(function() {
-		// console.log(this);
-		// var value = this;
-		// $(".pagination__form").attr('action', '?page=hello');
-	});
-});
