fc0eda0ed3f6ff6463cf470e940af273da1d48d9
kjacks <kjack227@gmail.com>
2016-03-31 11:51:24 -0400

    added pagination logic

---
+++ mysite/assets/js/modules/pagination.js
@@ -0,0 +1,9 @@
+import $ from 'jquery'
+
+$(document).ready(function() {
+	$(".pagination__form").submit(function() {
+		// console.log(this);
+		// var value = this;
+		// $(".pagination__form").attr('action', '?page=hello');
+	});
+});
