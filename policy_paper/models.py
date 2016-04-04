83b7e26688c39edeba9557d29680d1cc01e29b93
Sana Javed <sanajaved7@gmail.com>
2016-04-04 16:13:41 -0400

    Refactoring the PolicyPaper models to use the get_posts_and_programs in ProgramPolicyPapersPage so that it can be used for both Programs and Subprograms.

--- policy_paper/models.py
+++ policy_paper/models.py
@@ -4,15 +4,13 @@ from home.models import Post
 
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import StreamField
-from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel, FieldPanel
+from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel
 from wagtail.wagtailcore.blocks import URLBlock
 from wagtail.wagtaildocs.blocks import DocumentChooserBlock
-from wagtail.wagtailimages.models import Image
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
-from programs.models import Program
+from mysite.helpers import paginate_results, get_posts_and_programs
 
-from mysite.pagination import paginate_results
 
 class PolicyPaper(Post):
     """
@@ -31,16 +29,16 @@ class PolicyPaper(Post):
     )
 
     paper_url = StreamField([
-    	('policy_paper_url', URLBlock(required=False, null=True)),
+        ('policy_paper_url', URLBlock(required=False, null=True)),
     ])
 
     attachment = StreamField([
-    	('attachment', DocumentChooserBlock(required=False, null=True)),
+        ('attachment', DocumentChooserBlock(required=False, null=True)),
     ])
 
     content_panels = Post.content_panels + [
-    	StreamFieldPanel('paper_url'),
-    	StreamFieldPanel('attachment'),
+        StreamFieldPanel('paper_url'),
+        StreamFieldPanel('attachment'),
         ImageChooserPanel('publication_cover_image'),
     ]
 
@@ -56,7 +54,7 @@ class AllPolicyPapersHomePage(Page):
 
     def get_context(self, request):
         context = super(AllPolicyPapersHomePage, self).get_context(request)
-        all_posts = PolicyPaper.objects.all()
+        all_posts = PolicyPaper.objects.all().order_by("-date")
 
         context['all_posts'] = paginate_results(request, all_posts)
 
@@ -72,20 +70,16 @@ class ProgramPolicyPapersPage(Page):
     returns all Policy Papers associated with a specific
     Program which is determined using the url path
     """
-    parent_page_types = ['programs.Program'] 
+    parent_page_types = ['programs.Program', 'programs.Subprogram']
     subpage_types = ['PolicyPaper']
 
     def get_context(self, request):
-        context = super(ProgramPolicyPapersPage, self).get_context(request)
-        program_slug = request.path.split("/")[-3]
-        program = Program.objects.get(slug=program_slug)
-        all_posts = PolicyPaper.objects.filter(parent_programs=program)
-
-        context['all_posts'] = paginate_results(request, all_posts)
-
-        context['program'] = program
-
-        return context
+        return get_posts_and_programs(
+            self,
+            request,
+            ProgramPolicyPapersPage,
+            PolicyPaper
+        )
 
     class Meta:
-        verbose_name = "Policy Paper Homepage for Programs"
+        verbose_name = "Policy Paper Homepage for Programs and Subprograms"
