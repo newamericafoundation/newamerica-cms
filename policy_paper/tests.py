30dd8264db7a0d364b965b41460e2ffd56b87750
Sana Javed <sanajaved7@gmail.com>
2016-04-05 15:53:26 -0400

    Additional tests for Policy Paper models

--- policy_paper/tests.py
+++ policy_paper/tests.py
@@ -11,29 +11,41 @@ from .models import PolicyPaper, AllPolicyPapersHomePage, ProgramPolicyPapersPag
 
 
 class PolicyPaperTests(WagtailPageTests):
-	"""
-	Testing the Podcast, AllPodcastsHomePage, and
-    ProgramPodcastsPage models to confirm
+    """
+    Testing the PolicyPaper, AllPolicyPapersHomePage, and
+    ProgramPolicyPapersPage models to confirm
     hierarchies between pages and
     whether it is possible to create
     pages where it is appropriate.
 
-	"""
-	def setUp(self):
-		self.login()
-		self.root_page = Page.objects.get(id=1)
-		self.home_page = self.root_page.add_child(instance=HomePage(
+    """
+    def setUp(self):
+        self.login()
+        self.root_page = Page.objects.get(id=1)
+        self.home_page = self.root_page.add_child(instance=HomePage(
             title='New America')
         )
         self.all_policy_papers_home_page = self.home_page.add_child(
-        	instance=AllPolicyPapersHomePage(title='New America Policy Papers')
+            instance=AllPolicyPapersHomePage(title='New America Policy Papers')
         )
         self.program_page_1 = self.home_page.add_child(
             instance=Program(title='OTI', name='OTI', location=False, depth=3)
         )
-        self.program_policy_papers_page = self.program_page_1.add_child(instance=ProgramPodcastsPage(
+        self.program_policy_papers_page = self.program_page_1.add_child(instance=ProgramPolicyPapersPage(
                 title='OTI Policy Papers'))
         
-        self.podcast = self.program_podcasts_page.add_child(
-            instance=Podcast(title='Podcast 1', date='2016-02-10')
-        )
+        self.policy_paper = PolicyPaper(title='Policy Paper 1', slug='policy-paper-1', date='2016-02-10', depth=5)
+
+        self.program_policy_papers_page.add_child(
+            instance=self.policy_paper)
+        
+        self.policy_paper.save()
+
+    # Test that a child Page can be created under hte appropriate
+    # Parent Page
+    def test_can_create_policy_paper_under_program_policy_papers_page(self):
+        self.assertCanCreateAt(ProgramPolicyPapersPage, PolicyPaper)
+
+    def test_can_create_program_policy_papers_page_under_program(self):
+        self.assertCanCreateAt(ProgramPolicyPapersPage, PolicyPaper)
+
