0406fa8ba24edddc7d938839a8cce1c6dc3e013c
kjacks <kjack227@gmail.com>
2016-04-07 14:55:02 -0400

    added picture grid text interactive scroll on mouseover

---
+++ mysite/assets/js/modules/picture-grid/index.js
@@ -0,0 +1,23 @@
+import $ from 'jquery'
+
+var pictureGridTextHeight = 300;
+
+function addPictureGridInteraction() {
+	$(".picture-grid__entry__text-container").on("mouseover", function() {
+		var overflow = this.scrollHeight - pictureGridTextHeight;
+		if (overflow > 0) {
+			overflow = overflow + 15;
+			overflow = -1 * overflow;
+			$(this).css("transform", "translateY(" + overflow + "px)").css("overflow", "visible");
+		}
+
+	});
+
+	$(".picture-grid__entry__text-container").on("mouseout", function() {
+		$(this).css("transform", "translateY(0)").css("overflow", "hidden");
+	});
+}
+
+$( document ).ready(function() {
+	$(addPictureGridInteraction);
+});
