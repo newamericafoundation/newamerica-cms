f82847ad29594cee121f68fd7d957ce8d34bd588
kjacks <kjack227@gmail.com>
2016-04-07 10:59:04 -0400

    added function to pre-populate email field

--- mysite/assets/js/modules/subscribe.js
+++ mysite/assets/js/modules/subscribe.js
@@ -1,11 +1,20 @@
 import $ from 'jquery'
 
-function addSubscribeInteraction() {
-	$('.subscribe-form').submit(function() {
-		console.log("submitted");
-	});
+function populateEmailField() {
+	var email = getParameterByName('email');
+	$("#fieldEmail").val(email);
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
 $( document ).ready( function() {
-	$(addSubscribeInteraction);
+	$(populateEmailField);
 });
