9746b8347398753fc57c3f52210cc6e662a8dad7
kjacks <kjack227@gmail.com>
2016-04-07 10:04:25 -0400

    removed log statements from weekly-sidemenu js file

--- mysite/assets/js/modules/weekly-sidemenu/index.js
+++ mysite/assets/js/modules/weekly-sidemenu/index.js
@@ -5,18 +5,10 @@ import getJQueryObjects from './../../utilities/get_jquery_objects.js'
 function weeklyHighlightActiveLink() {
 	$('.weekly-sidemenu__sibling-list__link').each(function() {
 		var link = $(this).prop('href');
-		console.log(link);
 
 		if (link == window.location.href) {
-			console.log("adding active class");
 			$(this).addClass('active');
 		}
-
-		// console.log(window.location.href);
-		// if ($(this).prop('href') == window.location.href) {
-		// 	console.log("adding active class");
-		// 	$(this).addClass('active');
-		// }
 	});
 }
 
