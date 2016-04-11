07b8903a958c25752b6f3793bb0383720bb06b7a
Sana Javed <sanajaved7@gmail.com>
2016-04-11 17:46:49 -0400

    Updating the author transfer script

--- home/management/api/author_transfer_script.py
+++ home/management/api/author_transfer_script.py
@@ -18,6 +18,7 @@ from .article_transfer_script import load_articles, load_weekly_articles
 
 from .event_transfer_script import load_events
 
+from .book_transfer_script import load_books
 
 our_people_page = OurPeoplePage.objects.first()
 
@@ -28,7 +29,7 @@ if sys.version_info[0] < 3:
 
 def load_users_mapping():
     csv_data = {}
-    with open('home/management/api/authors.csv', "r") as csvfile:
+    with open('home/management/api/csv_scripts/authors.csv', "r") as csvfile:
         csv_reader = csv.reader(csvfile)
         for row in csv_reader:
             csv_data[str(row[0])] = {
@@ -66,13 +67,12 @@ def download_image(url, image_filename):
 def get_role_info(old_role):
 
     new_roles = {
-        "staff": 'Staff',
+        "staff": 'Central Staff',
         "board": 'Board Member',
-        "fellow": 'New America Fellow',
+        "fellow": 'Fellow',
         "alumni": 'External Author/Former Staff',
-        "programfellow": 'Program Fellow',
     }
-    expert_roles = ['Board Member', 'New America Fellow', 'Program Fellow']
+    expert_roles = ['Fellow']
     role_info = {}
 
     if not old_role:
@@ -101,60 +101,63 @@ def load_authors():
             mapped_user_title = '{0} {1}'.format(
                 mapped_user['first_name'],
                 mapped_user['last_name'])
-            print(mapped_user_title)
             mapped_user_slug = slugify(mapped_user_title)
             db_user = Person.objects.filter(slug=mapped_user_slug).first()
         
-        role_info = get_role_info(user_api['roles'])
-        if not db_user or mapped_user_slug:
-            db_user = Person(
-                    search_description='', 
-                    seo_title='', 
-                    show_in_menus=False,
-                    slug=mapped_user_slug,
-                    title=mapped_user_title, 
-                    first_name=mapped_user['first_name'], 
-                    last_name=mapped_user['last_name'], 
-                    position_at_new_america=user_api['title'],
-                    role=role_info['role_title'],
-                    email=user_api['email'],
-                    expert=role_info['expert'],
-                    depth=4,
-                    profile_image=download_image(
-                        user_api['image'],
-                        mapped_user_slug + "_image.jpeg"
-                    ),
-                    short_bio=user_api['short_bio'],
-                    long_bio=user_api['long_bio'],
+            role_info = get_role_info(user_api['roles'])
+            if not db_user and mapped_user_slug:
+                db_user = Person(
+                        search_description='', 
+                        seo_title='', 
+                        show_in_menus=False,
+                        slug=mapped_user_slug,
+                        title=mapped_user_title, 
+                        first_name=mapped_user['first_name'], 
+                        last_name=mapped_user['last_name'], 
+                        position_at_new_america=user_api['title'],
+                        role=role_info['role_title'],
+                        email=user_api['email'],
+                        expert=role_info['expert'],
+                        depth=4,
+                        profile_image=download_image(
+                            user_api['image'],
+                            mapped_user_slug + "_person_image.jpeg"
+                        ),
+                        short_bio=user_api['short_bio'],
+                        long_bio=user_api['long_bio'],
+                    )
+                our_people_page.add_child(instance=db_user)
+                db_user.save()
+                print("Adding new person: ")
+                print(db_user)
+            elif db_user and mapped_user_slug:
+                db_user.search_description=''
+                db_user.seo_title=''
+                db_user.show_in_menus=False
+                db_user.slug=mapped_user_slug
+                db_user.title=mapped_user_title
+                db_user.first_name=mapped_user['first_name']
+                db_user.last_name=mapped_user['last_name'] 
+                db_user.position_at_new_america=user_api['title']
+                db_user.role=role_info['role_title']
+                db_user.email=user_api['email']
+                db_user.expert=role_info['expert']
+                db_user.depth=4
+                db_user.short_bio=user_api['short_bio']
+                db_user.long_bio=user_api['long_bio']
+                db_user.profile_image=download_image(
+                    user_api['image'],
+                    mapped_user_slug + "_image.jpeg"
                 )
-            our_people_page.add_child(instance=db_user)
-            db_user.save()
-        else:
-            db_user.search_description=''
-            db_user.seo_title=''
-            db_user.show_in_menus=False
-            db_user.slug=mapped_user_slug
-            db_user.title=mapped_user_title
-            db_user.first_name=mapped_user['first_name']
-            db_user.last_name=mapped_user['last_name'] 
-            db_user.position_at_new_america=user_api['title']
-            db_user.role=role_info['role_title']
-            db_user.email=user_api['email']
-            db_user.expert=role_info['expert']
-            db_user.depth=4
-            db_user.short_bio=user_api['short_bio']
-            db_user.long_bio=user_api['long_bio']
-
-            db_user.profile_image=download_image(
-                user_api['image'],
-                mapped_user_slug + "_image.jpeg"
-            )
-            db_user.save()
+                db_user.save()
+                print("Updating existing person: ")
+                print(db_user)
 
 
 def run():
-    #load_authors()
+    load_authors()
     #load_articles()
     # load_events()
-    load_weekly_articles()
+    # load_weekly_articles()
+    # load_books()
 
