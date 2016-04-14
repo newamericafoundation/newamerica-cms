538427027bee21f8f3b3ce1ad5e402115c0a873e
Sana Javed <sanajaved7@gmail.com>
2016-04-14 18:18:03 -0400

    Almost finished with the policy paper transfer script. Majority of content moved over. Remaining is authors and URLs for papers

--- home/management/api/policy_paper_transfer_script.py
+++ home/management/api/policy_paper_transfer_script.py
@@ -8,11 +8,30 @@ from policy_paper.models import PolicyPaper, ProgramPolicyPapersPage
 
 from django.utils.text import slugify
 
-from transfer_script_helpers import download_image, get_post_date, get_summary, need_to_update_post, get_post_authors, get_program, get_content_homepage
+from transfer_script_helpers import download_image, get_post_date, get_summary, need_to_update_post, get_post_authors, get_program, get_content_homepage, download_document
+
+def get_attachments(attachments, title):
+    """
+    If there are attachments, it iterates through them
+    and moves them into the StreamField
+    """
+    if attachments:
+        content = []
+        for attachment in attachments:
+            content.append(
+                {
+                    'type': 'attachment',
+                    'value': download_document(
+                        attachment['attachment_url'],
+                        title
+                        )
+                }
+            )
+        return content
 
 
 def load_policy_papers():
-	"""
+    """
     Transfers all policy papers from the old database API
     for all programs into the new database
     creating objects of the PolicyPaper model
@@ -52,6 +71,13 @@ def load_policy_papers():
                                 'value': post['content']
                             }
                         ]),
+                        # paper_url=post['']
+                        attachment=json.dumps(
+                            get_attachments(
+                                post['attachments'],
+                                policy_paper_slug
+                            )
+                        ),
                         story_excerpt=get_summary(post['summary']),
                         story_image=download_image(
                             post['cover_image_url'], 
@@ -60,31 +86,37 @@ def load_policy_papers():
                     )
                     parent_program_policy_papers_homepage.add_child(instance=new_policy_paper)
                     new_policy_paper.save()
-                    get_post_authors(new_policy_paper, post['authors'])
-                elif new_book and book_slug and need_to_update_post(post['modified']):
-                    new_book.search_description = ''
-                    new_book.seo_title = ''
-                    new_book.depth = 5
-                    new_book.date = get_post_date(post['publish_at'])
-                    new_book.show_in_menus = False
-                    new_book.slug = book_slug
-                    new_book.title = post['title']
-                    new_book.body = json.dumps([
+                    # get_post_authors(new_policy_paper, post['authors'])
+                elif new_policy_paper and policy_paper_slug and need_to_update_post(post['modified']):
+                    new_policy_paper.search_description = ''
+                    new_policy_paper.seo_title = ''
+                    new_policy_paper.depth = 5
+                    new_policy_paper.date = get_post_date(post['publish_at'])
+                    new_policy_paper.show_in_menus = False
+                    new_policy_paper.slug = policy_paper_slug
+                    new_policy_paper.title = post['title']
+                    new_policy_paper.body = json.dumps([
                             {
                                 'type': 'paragraph',
                                 'value': post['content']
                             }
                         ])
-                    new_book.publication_cover_image = download_image(
-                            post['book_cover_image_url'], 
-                            book_slug + "_cover_image.jpeg"
+                    new_policy_paper.attachment=json.dumps(
+                            get_attachments(
+                                post['attachments'],
+                                policy_paper_slug
+                            )
                         )
-                    new_book.story_image = download_image(
+                    new_policy_paper.publication_cover_image = download_image(
                             post['cover_image_url'], 
-                            book_slug + "_image.jpeg"
+                            policy_paper_slug + "_cover_image.jpeg"
+                        )
+                    new_policy_paper.story_image = download_image(
+                            post['cover_image_url'], 
+                            policy_paper_slug + "_image.jpeg"
                     )
-                    new_book.subheading=post['sub_headline']
-                    new_book.save()
-                    get_post_authors(new_book, post['authors'])
+                    new_policy_paper.subheading=post['sub_headline']
+                    new_policy_paper.save()
+                    # get_post_authors(new_policy_paper, post['authors'])
             except django.db.utils.IntegrityError:
                 pass
