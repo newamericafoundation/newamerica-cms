427fe0bbe5a8fb6acf4d798a19c0aea227908c5e
kjacks <kjack227@gmail.com>
2016-04-19 15:11:37 -0400

    added logic to bold first paragraph

--- mysite/assets/js/modules/post-body/index.js
+++ mysite/assets/js/modules/post-body/index.js
@@ -1,26 +1,14 @@
 import $ from 'jquery'
 
-// removes dropcap if body height is less than cutoff
-function checkBodyHeight() {
-	var dropcapCutoff = 70;
-	var $post_body = $(".post-body");
-	var bodyHeight = $post_body.height();
-
-	if (bodyHeight < dropcapCutoff) {
-		$post_body.removeClass("with-dropcap");
+// removes dropcap if first paragraph character count is less than cutoff
+function checkDropcap() {
+	var dropcapCutoff = 300;
+	var $first_paragraph = $(".with-dropcap > .block-paragraph:first-child p:first-child");
+	var paragraphLength = $first_paragraph.text().length;
+	
+	if (paragraphLength < dropcapCutoff) {
+		$(".with-dropcap").removeClass("with-dropcap");
 	}
 }
 
-function styleFirstLine() {
-	var $first_paragraph = $(".with-dropcap .block-paragraph:first-child p:first-child");
-
-	var sentences = $first_paragraph.text().split('.');
-	
-	sentences[0] = "<span class=post-body__first-sentence>" + sentences[0] + ".</span>";
-	console.log(sentences);
-
-	$first_paragraph.html(sentences.join('.'))
-}
-
-$(checkBodyHeight);
-$(styleFirstLine);
+$(checkDropcap);
