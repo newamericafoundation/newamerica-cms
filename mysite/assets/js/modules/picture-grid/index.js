b1047773d5eaee305e122a40699b021e799e9623
kjacks <kjack227@gmail.com>
2016-04-22 12:06:32 -0400

    removed commented code

--- mysite/assets/js/modules/picture-grid/index.js
+++ mysite/assets/js/modules/picture-grid/index.js
@@ -6,22 +6,12 @@ var pictureGridEntryHeight = 500;
 var mediumBreakpoint = 640;
 
 var mediumMediaQuery = window.matchMedia("(min-width: " + mediumBreakpoint + "px)");
-// resizeMediaQuery.addListener(removePictureGridInteraction);
-// removePictureGridInteraction(resizeMediaQuery);
 
 function addPictureGridInteraction() {
 	$(".picture-grid__entry__text-container").on("mouseover", function() {
 		if (mediumMediaQuery.matches) {
 			var overflow = this.scrollHeight - pictureGridTextHeight;
 			if (overflow > 0) {
-				// if (overflow > pictureGridTextHeight) {
-				// 	overflow = pictureGridTextHeight;
-				// } else {
-				// 	overflow = overflow + 10;
-				// }
-				// $(this).children(".picture-grid__entry__text-fadeout").hide();
-				
-				// $(this).css("transform", "translateY(-" + overflow + "px)").css("height", this.scrollHeight - 25);
 				$(this).children(".picture-grid__entry__text-fadeout").hide();
 				$(this).css("transform", "translateY(-" + (pictureGridEntryHeight - pictureGridTextHeight) + "px)").css("height", pictureGridEntryHeight - 25);
 			}
