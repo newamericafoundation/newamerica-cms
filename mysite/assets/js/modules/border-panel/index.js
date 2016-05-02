9f550097461dbfb9226d41d668d9cc1e53fb22a4
kjacks <kjack227@gmail.com>
2016-05-02 13:35:32 -0400

    added dynamic resizing to border panels

--- mysite/assets/js/modules/border-panel/index.js
+++ mysite/assets/js/modules/border-panel/index.js
@@ -6,6 +6,28 @@ function addAllBorderPanelsInteractivity() {
 	$('.border-panel').each((i, el) => {
 		addBorderPanelInteractivity($(el))
 	})
+
+	var maxHeight = findMaxHeight();
+	$(".border-panel").height(maxHeight + 50);
+
+	console.log(maxHeight);
+	
+}
+
+function findMaxHeight() {
+	var maxHeight = 0;
+	$.each($('.border-panel__text'), function() {
+		if (maxHeight < $(this).height()) {
+			maxHeight = $(this).height();
+		}
+	});
+
+	return maxHeight;
 }
 
 $(addAllBorderPanelsInteractivity)
+
+$(window).resize(function(){
+	var maxHeight = findMaxHeight();
+	$(".border-panel").height(maxHeight + 50);
+});
