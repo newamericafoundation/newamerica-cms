125551ec01ae10a6bb19ed6f89fb570f9021e94c
Sana <sanajaved7@gmail.com>
2016-05-04 14:43:54 -0400

    Adding hosts

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -18,6 +18,8 @@ import os
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
+ALLOWED_HOSTS = ['*']
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
