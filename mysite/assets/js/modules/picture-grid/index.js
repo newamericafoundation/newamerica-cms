d635ae92be4aef95137c81097268da682c5b2211
kjacks <kjack227@gmail.com>
2016-05-05 17:02:52 -0400

    set state for no image picture grid

--- mysite/assets/js/modules/picture-grid/index.js
+++ mysite/assets/js/modules/picture-grid/index.js
@@ -8,7 +8,7 @@ var mediumBreakpoint = 640;
 var mediumMediaQuery = window.matchMedia("(min-width: " + mediumBreakpoint + "px)");
 
 function addPictureGridInteraction() {
-	$(".picture-grid__entry__text-container").on("mouseover", function() {
+	$(".picture-grid__entry__text-container.has-image").on("mouseover", function() {
 		if (mediumMediaQuery.matches) {
 			var overflow = this.scrollHeight - pictureGridTextHeight;
 			if (overflow > 0) {
@@ -18,7 +18,7 @@ function addPictureGridInteraction() {
 		}
 	});
 
-	$(".picture-grid__entry__text-container").on("mouseout", function() {
+	$(".picture-grid__entry__text-container.has-image").on("mouseout", function() {
 		if (mediumMediaQuery.matches) {
 			$(this).css("transform", "translateY(0)").css("height", pictureGridTextHeight - 25);
 			$(this).children(".picture-grid__entry__text-fadeout").show();
