7a005c7cd95366a7888041c915b00ae61f50aba7
kjacks <kjack227@gmail.com>
2016-05-12 15:27:35 -0400

    added script to replace http with https

--- mysite/assets/js/modules/post-body/index.js
+++ mysite/assets/js/modules/post-body/index.js
@@ -4,7 +4,7 @@ import $ from 'jquery';
 Removes dropcap if first paragraph character count is less than cutoff character limit
 
 */
-export default function checkDropcap() {
+function checkDropcap() {
 	var dropcapCutoff = 300;
 	var $first_paragraph = $(".with-dropcap > .block-paragraph:first-child p:first-child");
 	var paragraphLength = $first_paragraph.text().length;
@@ -12,4 +12,18 @@ export default function checkDropcap() {
 	if (paragraphLength < dropcapCutoff) {
 		$(".with-dropcap").removeClass("with-dropcap");
 	}
+}
+
+function convertToHttps() {
+	$('.post-body').find('iframe').each( function() {
+		var source = $(this).prop('src');
+		var newSource = source.replace('http:', 'https:');
+		$(this).prop('src', newSource);
+	});
+		
+}
+
+export default function() {
+	checkDropcap();
+	convertToHttps();
 }
