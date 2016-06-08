57c333aeb060f80455d3cae08ce68e1a1ffde1ae
kjacks <kjack227@gmail.com>
2016-06-08 17:50:20 -0400

    added template tag to check oti url

--- mysite/assets/js/modules/oti/index.js
+++ mysite/assets/js/modules/oti/index.js
@@ -8,11 +8,11 @@ Compares window location url to oti url - if they match adds oti class to conten
 const OTIurl = 'oti';
 
 export default function checkOTI() {
-	const url = window.location.href;
-	const urlPieces = url.split('/');
-	for (let i in urlPieces) {
-		if (urlPieces[i].toLowerCase() === OTIurl) {
-			$('.wrapper').addClass('oti');
-		}
-	}
+	// const url = window.location.href;
+	// const urlPieces = url.split('/');
+	// for (let i in urlPieces) {
+	// 	if (urlPieces[i].toLowerCase() === OTIurl) {
+	// 		$('.wrapper').addClass('oti');
+	// 	}
+	// }
 }
