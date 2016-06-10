9fa9946bfcb0c32d8eae8a90def8ad1aad01457d
kjacks <kjack227@gmail.com>
2016-06-10 09:59:09 -0400

    finalized fadeout

--- mysite/assets/js/modules/header/index.js
+++ mysite/assets/js/modules/header/index.js
@@ -1,28 +1,28 @@
 import $ from 'jquery';
 
 export default function setProgramDropdownHeight() {
-	if (!$("body").hasClass("header--expanded")) {
-		$("#program-dropdown-toggle").on("mouseover", function() {
-			var windowHeightMinusHeader = $(window).height() - 72;
-			var progDropdownHeight = $("#program-dropdown-content").innerHeight();
-			var progDropdownScrollHeight = $("#program-dropdown-content").prop("scrollHeight");
+	// if (!$("body").hasClass("header--expanded")) {
+	// 	$("#program-dropdown-toggle").on("mouseover", function() {
+	// 		var windowHeightMinusHeader = $(window).height() - 72;
+	// 		var progDropdownHeight = $("#program-dropdown-content").innerHeight();
+	// 		var progDropdownScrollHeight = $("#program-dropdown-content").prop("scrollHeight");
 
-			console.log("window height: " + windowHeightMinusHeader);
-			console.log("prog dropdown height: " + progDropdownHeight);
-			console.log("prog dropdown scroll height: " + progDropdownScrollHeight);
+	// 		console.log("window height: " + windowHeightMinusHeader);
+	// 		console.log("prog dropdown height: " + progDropdownHeight);
+	// 		console.log("prog dropdown scroll height: " + progDropdownScrollHeight);
 
-			if (progDropdownScrollHeight > progDropdownHeight) {
-				console.log("overflow!");
-				$("#program-dropdown-content").innerHeight("auto");
-				progDropdownHeight = $("#program-dropdown-content").innerHeight();
-			}
+	// 		if (progDropdownScrollHeight > progDropdownHeight) {
+	// 			console.log("overflow!");
+	// 			$("#program-dropdown-content").innerHeight("auto");
+	// 			progDropdownHeight = $("#program-dropdown-content").innerHeight();
+	// 		}
 
-			if (progDropdownHeight > windowHeightMinusHeader) {
-				console.log("prog dropdown height is greater!");
-				$("#program-dropdown-content").height(windowHeightMinusHeader - 75);
-			}
+	// 		if (progDropdownHeight > windowHeightMinusHeader) {
+	// 			console.log("prog dropdown height is greater!");
+	// 			$("#program-dropdown-content").height(windowHeightMinusHeader - 75);
+	// 		}
 
-		})
-	}
+	// 	})
+	// }
 	
 }
