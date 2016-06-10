174eae65fb40aa4cc4055701daa3e56179a7c74a
kjacks <kjack227@gmail.com>
2016-06-10 11:15:20 -0400

    resolved firefox and IE bottom padding cutoff problem

--- mysite/assets/js/modules/header/index.js
+++ mysite/assets/js/modules/header/index.js
@@ -1,28 +1,29 @@
 import $ from 'jquery';
 
 export default function setProgramDropdownHeight() {
-	// if (!$("body").hasClass("header--expanded")) {
-	// 	$("#program-dropdown-toggle").on("mouseover", function() {
-	// 		var windowHeightMinusHeader = $(window).height() - 72;
-	// 		var progDropdownHeight = $("#program-dropdown-content").innerHeight();
-	// 		var progDropdownScrollHeight = $("#program-dropdown-content").prop("scrollHeight");
+	if (!$("body").hasClass("header--expanded")) {
+		$("#program-dropdown-toggle").on("mouseover", function() {
+			var windowHeightMinusHeader = $(window).height() - 72;
+			var progDropdownHeight = $("#program-dropdown-content").height();
+			var progDropdownScrollHeight = $("#program-dropdown-content").prop("scrollHeight");
 
-	// 		console.log("window height: " + windowHeightMinusHeader);
-	// 		console.log("prog dropdown height: " + progDropdownHeight);
-	// 		console.log("prog dropdown scroll height: " + progDropdownScrollHeight);
+			console.log("window height: " + windowHeightMinusHeader);
+			console.log("prog dropdown height: " + progDropdownHeight);
+			console.log("prog dropdown scroll height: " + progDropdownScrollHeight);
 
-	// 		if (progDropdownScrollHeight > progDropdownHeight) {
-	// 			console.log("overflow!");
-	// 			$("#program-dropdown-content").innerHeight("auto");
-	// 			progDropdownHeight = $("#program-dropdown-content").innerHeight();
-	// 		}
+			if (progDropdownScrollHeight > progDropdownHeight) {
+				console.log("overflow!");
+				$("#program-dropdown-content").height("auto");
+				progDropdownHeight = $("#program-dropdown-content").height();
+			}
 
-	// 		if (progDropdownHeight > windowHeightMinusHeader) {
-	// 			console.log("prog dropdown height is greater!");
-	// 			$("#program-dropdown-content").height(windowHeightMinusHeader - 75);
-	// 		}
+			if (progDropdownHeight > windowHeightMinusHeader) {
+				console.log("prog dropdown height is greater!");
+				$("#program-dropdown-content").height(windowHeightMinusHeader - 15).css("overflowY", "scroll");
 
-	// 	})
-	// }
+			}
+
+		})
+	}
 	
 }
