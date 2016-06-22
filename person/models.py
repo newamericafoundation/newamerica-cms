641e16aac010e42c525e330fdaf7728c0ee8fffa
Sana <sanajaved7@gmail.com>
2016-06-22 11:29:28 -0400

    Added pagination to content on bio page

--- person/models.py
+++ person/models.py
@@ -207,7 +207,7 @@ class Person(Page):
         ]
 
         context['featured_work'] = featured_work
-        context['posts'] = self.post_set.all().order_by("-date")
+        context['posts'] = paginate_results(request,self.post_set.all().order_by("-date"))
                 
         return context
 
