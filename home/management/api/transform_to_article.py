9a3a64dc340f2f5c6324ef40103729564836c715
Sana Javed <sanajaved7@gmail.com>
2016-06-28 11:52:31 -0400

    Working on script

--- home/management/api/transform_to_article.py
+++ home/management/api/transform_to_article.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import django.db.utils
 
-import json
+import simplejson as json
 
 import csv
 import json
@@ -10,6 +10,8 @@ import io
 from blog.models import BlogPost, ProgramBlogPostsPage
 
 from django.core.exceptions import ObjectDoesNotExist
+
+from django.utils.text import slugify
 
 from .newamerica_api_client import NAClient
 
@@ -52,123 +54,136 @@ def get_source_url(source_url):
         except ValidationError:
             pass
 
+# using production database to find old in the news posts and create 
+def transform_itn_to_articles():
+    article_mapping = inthenews_to_article_mapping()
 
-# def transform_itn_to_articles():
-#     article_mapping = inthenews_to_article_mapping()
+    for item in article_mapping:
+        print article_mapping[item]['id']
+        slug = slugify(article_mapping[item]['title'])
+        print(slug)
+        old_post = Quoted.objects.filter(slug=slug).first()
+        if old_post:
+            try:
+                print("found existing post")
+                parent_program = old_post.get_ancestors()[2]
+                parent_program_articles_homepage = get_content_homepage(
+                        parent_program, 
+                        ProgramArticlesPage,
+                        'Articles',
+                )
+                new_post = Article(
+                    title=old_post.title,
+                    date=old_post.date,
+                    slug=old_post.slug,
+                    body=old_post.body,
+                    depth=5,
+                    programs=old_post.programs.all(),
+                    source=old_post.source,
+                    source_url=old_post.source_url,
+                    authors=old_post.authors.all(),
+                )
+                parent_program_articles_homepage.add_child(instance=new_post)
+                new_post.save()
+                print("saved new post")
+                old_post.delete()
+                print("deleted old post")
+            except django.db.utils.IntegrityError:
+                pass
+        else:
+            print('did not find post here')
 
-#     for item in article_mapping:
-#         slug = article_mapping[item]['slug']
-#         print(slug)
-#         post = Quoted.objects.filter(slug=slug).first()
-#         if post:
-#             parent_program = post.get_ancestors()[2]
-#             parent_program_articles_homepage = get_content_homepage(
-#                     parent_program, 
-#                     ProgramArticlesPage,
-#                     'Articles',
-#             )
-#             new_post = Article(
-#                 title=post.title,
-#                 date=post.date,
-#                 slug=post.slug,
-#                 body=post.body,
-#                 depth=5,
-#                 programs=post.programs.all(),
-#                 source=post.source,
-#                 source_url=post.source_url,
-#             )
-#             parent_program_articles_homepage.add_child(new_post)
-#             new_post.save()
-#         else:
-#             print('did not find post here')
-#             break
 
 def get_parent_for_itn(parent):
     parent = parent.split(",")[0]
 
     return parent
 
-# to create the ITN pieces to replicate process in staging + production 
-def transform_itn_to_articles():
-    article_mapping = inthenews_to_article_mapping()
+# using old API - to create the ITN pieces to replicate process in staging + production 
+# def transform_itn_to_articles():
+#     article_mapping = inthenews_to_article_mapping()
 
-    for item in article_mapping:
-        print article_mapping[item]['id']
-        client = NAClient()
-        post = client.get_one_in_the_news_piece(article_mapping[item]['id'])
-        
-        try:
-            program_id = get_parent_for_itn(article_mapping[item]['parent'])
-            print(program_id)
-            post_parent_program = get_program(program_id)
-            
-            parent_program_quoted_homepage = get_content_homepage(
-                post_parent_program, 
-                ProgramQuotedPage,
-                'In The News',
-            )
+#     for item in article_mapping:
+#         print article_mapping[item]['id']
+#         try:
+#             client = NAClient()
+#             post = client.get_one_in_the_news_piece(article_mapping[item]['id'])
+#             if article_mapping[item]['id'] != "id":
+#                 try:
+#                     program_id = get_parent_for_itn(article_mapping[item]['parent'])
+#                     print(program_id)
+#                     post_parent_program = get_program(program_id)
+                    
+#                     parent_program_quoted_homepage = get_content_homepage(
+#                         post_parent_program, 
+#                         ProgramQuotedPage,
+#                         'In The News',
+#                     )
 
-            article_slug = post['slug']
+#                     article_slug = slugify(post['title'])
 
-            new_article = Quoted.objects.filter(slug=article_slug).first()
+#                     new_article = Quoted.objects.filter(slug=article_slug).first()
 
-            if not new_article and article_slug:
-                new_article = Quoted(
-                    search_description='',
-                    seo_title='',
-                    depth=5,
-                    show_in_menus=False,
-                    slug=article_slug,
-                    title=post['title'],
-                    date=get_post_date(post['publish_at']),
-                    subheading=post['sub_headline'],
-                    body=json.dumps([
-                        {
-                            'type': 'paragraph',
-                            'value': post['content']
-                        }
-                    ]),
-                    story_excerpt=get_summary(post['summary']),
-                    story_image=download_image(
-                        post['cover_image_url'], 
-                        article_slug + "_image.jpeg"
-                    ),
-                    source=post['source'],
-                    source_url=post['source_url'],
-                )
-                parent_program_quoted_homepage.add_child(instance=new_article)
-                new_article.save()
-                #get_post_authors(new_article, post['authors'])
-                connect_programs_to_post(new_article, post['programs'])
-            elif new_article and article_slug:
-                new_article.search_description = ''
-                new_article.seo_title = ''
-                new_article.depth = 5
-                new_article.date = get_post_date(post['publish_at'])
-                new_article.show_in_menus = False
-                new_article.slug = article_slug
-                new_article.title = post['title']
-                new_article.body = json.dumps([
-                        {
-                            'type': 'paragraph',
-                            'value': post['content']
-                        }
-                    ])
-                new_article.story_image = download_image(
-                        post['cover_image_url'], 
-                        article_slug + "_image.jpeg"
-                )
-                new_article.subheading=post['sub_headline']
-                new_article.source=post['source']
-                new_article.source_url=post['source_url']
-                new_article.save()
-                #get_post_authors(new_article, post['authors'])
-                connect_programs_to_post(new_article, post['programs'])
-        except django.db.utils.IntegrityError:
-            pass
+#                     if not new_article and article_slug:
+#                         new_article = Quoted(
+#                             search_description='',
+#                             seo_title='',
+#                             depth=5,
+#                             show_in_menus=False,
+#                             slug=article_slug,
+#                             title=post['title'],
+#                             date=get_post_date(post['publish_at']),
+#                             subheading=post['sub_headline'],
+#                             body=json.dumps([
+#                                 {
+#                                     'type': 'paragraph',
+#                                     'value': post['content']
+#                                 }
+#                             ]),
+#                             story_excerpt=get_summary(post['summary']),
+#                             story_image=download_image(
+#                                 post['cover_image_url'], 
+#                                 article_slug + "_image.jpeg"
+#                             ),
+#                             source=post['source'],
+#                             source_url=post['source_url'],
+#                         )
+#                         parent_program_quoted_homepage.add_child(instance=new_article)
+#                         new_article.save()
+#                         #get_post_authors(new_article, post['authors'])
+#                         connect_programs_to_post(new_article, post['programs'])
+#                     elif new_article and article_slug:
+#                         new_article.search_description = ''
+#                         new_article.seo_title = ''
+#                         new_article.depth = 5
+#                         new_article.date = get_post_date(post['publish_at'])
+#                         new_article.show_in_menus = False
+#                         new_article.slug = article_slug
+#                         new_article.title = post['title']
+#                         new_article.body = json.dumps([
+#                                 {
+#                                     'type': 'paragraph',
+#                                     'value': post['content']
+#                                 }
+#                             ])
+#                         new_article.story_image = download_image(
+#                                 post['cover_image_url'], 
+#                                 article_slug + "_image.jpeg"
+#                         )
+#                         new_article.subheading=post['sub_headline']
+#                         new_article.source=post['source']
+#                         new_article.source_url=post['source_url']
+#                         new_article.save()
+#                         #get_post_authors(new_article, post['authors'])
+#                         connect_programs_to_post(new_article, post['programs'])
+#                 except django.db.utils.IntegrityError:
+#                     pass
+#         except ValueError:
+#             pass
 
 
-# to actually delete the ITN pieces and create articles
+
+# using old API - to actually delete the ITN pieces and create articles
 # def transform_itn_to_articles():
 #     article_mapping = inthenews_to_article_mapping()
 
