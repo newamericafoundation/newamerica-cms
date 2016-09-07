ae00fdc3703470e8bdce312a372c8850d0e9654f
kjacks <kjack227@gmail.com>
2016-09-07 18:36:37 -0400

    set scrollbar padding for chart wrapper

--- mysite/assets/js/modules/dataviz/index.js
+++ mysite/assets/js/modules/dataviz/index.js
@@ -2,16 +2,22 @@ import $ from 'jquery';
 
 export default function detectOverflow() {
 	$(document).ready(setChartAreaWidth);
-
-	$(window).resize(setChartAreaWidth);
+	$(window).resize(function() { setChartAreaWidth(); setOverflowPadding(); });
 }
 
 function setChartAreaWidth() {
 	var bodyWidth = $("body").width();
 	$(".chart-wrapper").css("max-width", bodyWidth - 50);
+}
 
-	console.log($(".chart-wrapper").scrollWidth);
-	if ($(".dataviz__chart-wrapper").offsetWidth < $(".dataviz__chart-wrapper").scrollWidth) {
-		console.log("overflowing!!!");
-	}
+function setOverflowPadding() {
+	$(".chart-wrapper").each( function() {
+		var $chartWrapper = $(this);
+		if ($chartWrapper.prop("offsetWidth") < $chartWrapper.prop("scrollWidth")) {
+			$chartWrapper.css("padding-bottom", "15px");
+		} else {
+			$chartWrapper.css("padding-bottom", "0px");
+		}
+	});
+	
 }
