cb3185c87b92d5d714e69bfd8675932c25f85f5f
kjacks <kjack227@gmail.com>
2016-10-14 11:59:54 -0400

    added print icon to cahrt download

--- mysite/assets/js/modules/dataviz/index.js
+++ mysite/assets/js/modules/dataviz/index.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 
 export default function detectOverflow() {
 	$(document).ready(setChartAreaWidth);
+	$(document).ready(addDownloadPopupInteractivity);
 	$(window).resize(function() { setChartAreaWidth(); setOverflowPadding(); });
 }
 
@@ -24,4 +25,26 @@ function setOverflowPadding() {
 		}
 	});
 	
+}
+
+function addDownloadPopupInteractivity() {
+	$(".dataviz__download-link").each(function(index, item) {
+		console.log(item);
+		$(item).on("click", function() {
+			var popup = $(item).siblings(".dataviz__download-popup");
+			popup.toggle();
+		})
+
+		$('body').click(function(evt){
+			console.log(evt.target);  
+			if(evt.target.class == "dataviz__download-popup" || evt.target.class == "dataviz__download-link")
+				return;
+			// excepts descendents of download link and download popup
+			if($(evt.target).closest('.dataviz__download-popup').length || $(evt.target).closest('.dataviz__download-link').length)
+				return; 
+
+			$(".dataviz__download-popup").hide();
+		});
+
+	})
 }
