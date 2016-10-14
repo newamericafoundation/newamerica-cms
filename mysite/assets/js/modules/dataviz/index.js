7f4f62bace7e82aca4a763b445bceb8868dfa2a1
kjacks <kjack227@gmail.com>
2016-10-14 13:50:27 -0400

    added social shares to dataviz block

--- mysite/assets/js/modules/dataviz/index.js
+++ mysite/assets/js/modules/dataviz/index.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 
 export default function detectOverflow() {
 	$(document).ready(setChartAreaWidth);
-	$(document).ready(addDownloadPopupInteractivity);
+	$(document).ready(addSharePopupInteractivity);
 	$(window).resize(function() { setChartAreaWidth(); setOverflowPadding(); });
 }
 
@@ -27,24 +27,53 @@ function setOverflowPadding() {
 	
 }
 
-function addDownloadPopupInteractivity() {
-	$(".dataviz__download-link").each(function(index, item) {
+function addSharePopupInteractivity() {
+	$(".dataviz__share-link").each(function(index, item) {
 		console.log(item);
+		var popup = $(item).siblings(".dataviz__share-popup");
 		$(item).on("click", function() {
-			var popup = $(item).siblings(".dataviz__download-popup");
 			popup.toggle();
 		})
 
 		$('body').click(function(evt){
 			console.log(evt.target);  
-			if(evt.target.class == "dataviz__download-popup" || evt.target.class == "dataviz__download-link")
+			if(evt.target.class == "dataviz__share-popup" || evt.target.class == "dataviz__share-link")
 				return;
-			// excepts descendents of download link and download popup
-			if($(evt.target).closest('.dataviz__download-popup').length || $(evt.target).closest('.dataviz__download-link').length)
+			// excepts descendents of share link and share popup
+			if($(evt.target).closest('.dataviz__share-popup').length || $(evt.target).closest('.dataviz__share-link').length)
 				return; 
 
-			$(".dataviz__download-popup").hide();
+			$(".dataviz__share-popup").hide();
 		});
 
+		addShareIcons(popup);
+
 	})
+}
+
+function addShareIcons(popup) {
+	var sectionId = $(popup).closest("section")[0].id;
+	var urlPieces = window.location.href.split("#");
+	var currUrl = urlPieces[0];
+	console.log(currUrl);
+
+	$(popup).jsSocials({
+		url: currUrl + "#" + sectionId,
+    	showCount: true,
+		showLabel: true,
+		text: "",
+		shareIn: "popup",
+        shares: [
+        	{
+        		share: "email",
+	        	label: "Email",
+			    logo: "fa fa-envelope",
+			}, 
+			"twitter",
+			{
+				share: "facebook",
+			    label: "Share",
+			    logo: "fa fa-facebook"
+			}]
+    });
 }
