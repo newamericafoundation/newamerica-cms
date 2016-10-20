aa78226fda9d985ea718dc3b6e40c3e6da30e6fb
kjacks <kjack227@gmail.com>
2016-10-20 10:29:48 -0400

    moved chart button interactivity to website repo

--- mysite/assets/js/modules/dataviz/index.js
+++ mysite/assets/js/modules/dataviz/index.js
@@ -2,11 +2,14 @@ import $ from 'jquery';
 
 var browser = require('detect-browser');
 
+import domtoimage from 'dom-to-image';
+
+var printWidth = 725;
+
 export default function() {
 	$(document).ready(function() {
 		setChartWrapperWidth(); 
-		addShareInteractivity(); 
-		hideDownloadLinks();
+		addChartButtonInteractivity(); 
 	});
 	$(window).resize(function() { 
 		setChartWrapperWidth(); 
@@ -34,11 +37,63 @@ function setChartWrapperOverflowPadding() {
 	});
 }
 
+function addChartButtonInteractivity() {
+	$(".dataviz").each(function(index, item) {
+		var chartArea = $(item).find(".dataviz__chart-area")[0];
+		var downloadLink = $(item).find(".dataviz__download-link");
+		var printLink = $(item).find(".dataviz__print-link");
+		var shareLink = $(item).find(".dataviz__share-link");
+		
+		downloadLink.click(function() { handlePrintDownloadClickEvent("download", chartArea) });
+		printLink.click(function() { handlePrintDownloadClickEvent("print", chartArea) });
+
+		// hides print and download links on Internet Explorer
+		hidePrintDownloadLinks(downloadLink, printLink);
+
+		addShareInteractivity(shareLink, chartArea);
+	});
+}
+
+function handlePrintDownloadClickEvent(eventType, chartArea) {
+	domtoimage.toPng(chartArea)
+	    .then((dataUrl) => {
+	    	if (eventType == "download") {
+	    		downloadChart(dataUrl, chartArea);
+	    	} else {
+	    		printChart(dataUrl, chartArea);
+	    	}
+	    })
+	    .catch(function (error) {
+	        console.error('oops, something went wrong!', error);
+	    });
+}
+
+function downloadChart(dataUrl, chartArea) {
+	var id = $(chartArea).attr("id");
+	var link = document.createElement("a");
+    link.download = id + '.png';
+    link.href = dataUrl;
+    link.click();
+    link.remove();
+}
+
+function printChart(dataUrl, chartArea) {
+	var currWidth = $(chartArea).width();
+	var currHeight = $(chartArea).height();
+	var aspect = currHeight/currWidth;
+
+    var adjustedHeight = ( printWidth * aspect) + "px";
+    var popup = window.open();
+	popup.document.write("<img src=" + dataUrl + " height=" + adjustedHeight + " width=" + printWidth + "px></img>");
+	popup.focus(); //required for IE
+	popup.print();
+}
+
 // hides download links on Internet Explorer and shows alternative download data message
-function hideDownloadLinks() {
+function hidePrintDownloadLinks(downloadLink, printLink) {
 	if (browser.name == "ie") {
-		$(".dataviz__download-link").hide();
-		$(".dataviz__print-link").hide();
+		$(downloadLink).hide();
+		$(printLink).hide();
 		$(".in-depth__footer__download-data-message").show();
 	} else if (browser.name == "safari") {
 		$(".in-depth__footer__download-data-message").show();
@@ -46,25 +101,22 @@ function hideDownloadLinks() {
 }
 
 // adds click toggle handlers to share popup 
-function addShareInteractivity() {
-	$(".dataviz__share-link").each(function(index, item) {
-		var popup = $(item).siblings(".dataviz__share-popup");
-		$(item).on("click", function() {
-			popup.toggle();
-		})
+function addShareInteractivity(shareLink, chartArea) {
+	var popup = $(shareLink).siblings(".dataviz__share-popup");
+	$(shareLink).click(function() { popup.toggle(); });
 
-		$('body').click(function(evt){
-			if(evt.target.class == "dataviz__share-popup" || evt.target.class == ".dataviz__share-link")
-				return;
-			// excepts descendents of share link and share popup
-			if($(evt.target).closest('.dataviz__share-popup').length || $(evt.target).closest('.dataviz__share-link').length)
-				return; 
+	$('body').click(function(evt){
+		if(evt.target.class == "dataviz__share-popup" || evt.target.class == ".dataviz__share-link")
+			return;
+		// excepts descendents of share link and share popup
+		if($(evt.target).closest('.dataviz__share-popup').length || $(evt.target).closest('.dataviz__share-link').length)
+			return; 
 
-			$(".dataviz__share-popup").hide();
-		});
-		addShareIcons(popup);
-	})
+		$(".dataviz__share-popup").hide();
+	});
+	addShareIcons(popup);
 }
+
 
 function addShareIcons(popup) {
 	var urlPieces = window.location.href.split("#");
