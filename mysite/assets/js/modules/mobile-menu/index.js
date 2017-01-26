23e99c34b4dfaa41af5cb0477d8a3b11a81a3317
LomaxRx <andrewlomaxr@gmail.com>
2017-01-26 14:36:13 -0500

    replace mobile expand/collapse js logic with css

--- mysite/assets/js/modules/mobile-menu/index.js
+++ mysite/assets/js/modules/mobile-menu/index.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 /*
 
-Adds mobile menu interaction 
+Adds mobile menu interaction
     - toggles display of mobile menu and hides/shows page content when menu toggle arrows are clicked
     - calls function to add resize listener to ensure that if window is resized while mobile menus are displayed,
     	page content is displayed when out of mobile mode
@@ -12,53 +12,20 @@ Adds mobile menu interaction
 let headerMenuExpanded = false;
 let sideMenuExpanded = false;
 
-export default function addMenuResponsivity() {
-	$('.mobile-nav__main').on('click', function() {
-		// toggle mobile menu arrow for main, ensure arrow up for secondary
-		$('.mobile-nav__toggle__main').toggleClass('down');
-		$('.mobile-nav__toggle__secondary').removeClass('down');
-
-		// toggle header menu and ensure mobile sidemenu hidden
-		$('.mobile-header').toggle();
-		$('.sidemenu-container').css('display', 'none');
-		sideMenuExpanded = false;
-
-		// if expanding header, hide content and footer, else show content and footer
-		if (headerMenuExpanded) {
-			$('.content-container').css('display', 'table-cell');
-			$('footer').css('display', 'block');
-			headerMenuExpanded = false;
-		} else {
-			$('.content-container').css('display', 'none');
-			$('footer').css('display', 'none');
-			headerMenuExpanded = true;
-		}
-
+export default function menuToggle(){
+	$('.mobile-nav__main').click(function(){
+		$('body').toggleClass('mobile-menu--expanded')
+			.removeClass('secondary-menu--expanded');
 	});
 
-	$('.mobile-nav__secondary').on('click', function() {
-		// toggle mobile menu arrow for secondary, ensure arrow up for main
-		$('.mobile-nav__toggle__secondary').toggleClass('down');
-		$('.mobile-nav__toggle__main').removeClass('down');
-
-		// toggle sidemenu and ensure mobile header menu hidden
-		$('.mobile-sidemenu').css('display', 'block');
-		$('.mobile-header').css('display', 'none');
-		headerMenuExpanded = false;
-
-		// if expanding mobile sidemenu, hide content and footer, else show content and footer
-		if (sideMenuExpanded) {
-			$('.sidemenu-container').css('display', 'none');
-			$('.content-container').css('display', 'table-cell');
-			$('footer').css('display', 'block');
-			sideMenuExpanded = false;
-		} else {
-			$('.sidemenu-container').css('display', 'table-cell');
-			$('.content-container').css('display', 'none');
-			$('footer').css('display', 'none');
-			sideMenuExpanded = true;
-		}
+	$('.mobile-nav__secondary').click(function(){
+		$('body').toggleClass('secondary-menu--expanded')
+			.removeClass('mobile-menu--expanded');
 	});
+}
+
+export function addMenuResponsivity() {
+
 
 	addResizeListener();
 
@@ -87,7 +54,7 @@ Function called by resize listener to hide/show appropriate elements
 */
 function WidthChange(mq) {
 	if (mq.matches) {
-		// screen width is greater than breakpoint -> hide mobile elements, make sure page content elements are displayed, 
+		// screen width is greater than breakpoint -> hide mobile elements, make sure page content elements are displayed,
 		//		toggle menu arrows back up
 		$('.mobile-nav').css('display', 'none');
 		$('.mobile-header').css('display', 'none');
