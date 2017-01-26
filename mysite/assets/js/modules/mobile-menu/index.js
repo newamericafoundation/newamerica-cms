92076728eb3ea6e47a18d91d122983b5777faf83
LomaxRx <andrewlomaxr@gmail.com>
2017-01-26 15:15:40 -0500

    change flip-arrow js logic for css logic

--- mysite/assets/js/modules/mobile-menu/index.js
+++ mysite/assets/js/modules/mobile-menu/index.js
@@ -2,30 +2,25 @@ import $ from 'jquery';
 
 export default function menuToggle(){
 	$('.mobile-nav__main').click(function(){
-		$('body').toggleClass('mobile-menu--expanded')
+		$(this).toggleClass('active-nav')
+			.siblings().removeClass('active-nav');
+
+		$('body')
+			.toggleClass('mobile-menu--expanded')
 			.removeClass('secondary-menu--expanded');
 	});
 
 	$('.mobile-nav__secondary').click(function(){
-		$('body').toggleClass('secondary-menu--expanded')
+		$(this).toggleClass('active-nav')
+			.siblings().removeClass('active-nav');
+
+		$('body')
+			.toggleClass('secondary-menu--expanded')
 			.removeClass('mobile-menu--expanded');
 	});
 
-	addMobileMenuArrowInteraction()
-}
+	$('.mobile-menu__link__main a').click(function() {
+		$(this).toggleClass('active-nav');
+	});
 
-function addMobileMenuArrowInteraction() {
-	$('.mobile-menu__link__main a')
-		.on('click', function() {
-			$(this).siblings('.rotate').toggleClass('down');
-		});
-
-	// sets fill change for secondary menu arrows on hover
-	$('.mobile-header .mobile-menu__link__main a')
-		.on('mouseover', function() {
-			$(this).siblings('.rotate').removeClass('flip-arrow-black').addClass('flip-arrow-white');
-		})
-		.on('mouseout', function() {
-			$(this).siblings('.rotate').removeClass('flip-arrow-white').addClass('flip-arrow-black');
-		});
 }
