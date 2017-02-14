693f3115898150e54729cd2163d7b65d366026aa
LomaxRx <andrewlomaxr@gmail.com>
2017-02-14 12:43:01 -0500

    let scrollr handle looping through selection on conference

--- mysite/assets/js/modules/conference/index.js
+++ mysite/assets/js/modules/conference/index.js
@@ -1,57 +1,53 @@
 import $ from 'jquery';
 import scrollr from '../../utilities/scrollr.js';
-import expand from './speaker-expand.js';
+import speakerSetup from './speaker-expand.js';
 
 export default function() {
-  $(document).ready(function(){
 
-    expand();
-    let navItems = $('.conference-template .navigation-item a, .conference-template .go-to-about');
-    let isScrolling = false;
+  speakerSetup();
 
-    navItems.click(function(e){
-      e.preventDefault();
-      isScrolling = true;
-      let id = $(this).attr('href');
-      scrollr.smoothScroll(id,-75, ()=>{
-        isScrolling=false;
-        activeNav($(this));
-      });
+  if(!$('body').hasClass('conference-template')) return;
+
+  let navItems = $('.conference-template .navigation-item a, .conference-template .go-to-about');
+  let isScrolling = false;
+
+  navItems.click(function(e){
+    e.preventDefault();
+    isScrolling = true;
+    let id = $(this).attr('href');
+    scrollr.smoothScroll(id,-75, ()=>{
+      isScrolling=false;
+      activeNav($(this));
     });
+  });
 
-    $('.conference-template section.section-nav').each(function(){
-      let id = this.getAttribute('id');
-      let navItem = $(`.navigation-item [href^="#${id}"]`);
+  scrollr
+    .addTrigger('.conference-template section.section-nav', {
+      onEnter: function($el){
+        if(isScrolling) return;
 
-      scrollr.addTrigger(this,{
-        onEnter: function(el,trigger){
-          if(isScrolling) return;
-          activeNav(navItem);
-        },
-        offset: -75
-      });
-    });
-
-    if(!$('body').hasClass('conference-template')) return;
-
-    scrollr.addTrigger('#hero',{
-      hasLeft: function(el,trigger){
+        let id = $el.attr('id');
+        let navItem = $(`.navigation-item [href^="#${id}"]`);
+        activeNav(navItem);
+      },
+      offset: -75
+    })
+    .addTrigger('#hero',{
+      hasLeft: function(){
           $('body').addClass('fixed');
       },
-      onEnter: function(el,trigger){
+      onEnter: function($el,trigger){
         if(trigger.window.direction === "REVERSE")
           $('body').removeClass('fixed');
       }
     });
 
-    function activeNav($nav){
-      navItems.removeClass('active')
-      $nav.addClass('active');
-      $('.navbar').stop().animate(
-        {'scrollLeft':$nav.offset().left-15},
-        350
-      );
-    }
-
-  });
+  function activeNav($nav){
+    navItems.removeClass('active')
+    $nav.addClass('active');
+    $('.navbar').stop().animate(
+      {'scrollLeft':$nav.offset().left-15},
+      350
+    );
+  }
 }
