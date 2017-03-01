46246d287cb5b0d36a813ee34512f2737191a02b
LomaxRx <andrewlomaxr@gmail.com>
2017-03-01 12:21:56 -0500

    complete middleware

--- mysite/log_handlers.py
+++ mysite/log_handlers.py
@@ -1,4 +1,4 @@
-import logging, traceback, os
+import logging, traceback, os, sys
 from logdna import LogDNAHandler as LogDNA
 
 class LogDNAMiddleware(object):
@@ -19,27 +19,23 @@ class LogDNAMiddleware(object):
         response = self.get_response(request)
         return response
 
-    def process_exception(request, exception):
-        print exception
-        try:
-            if record:
-                if record.exc_info is not None:
-                    exc_type, exc_val, exc_tb = record.exc_info
-                    tb = traceback.extract_tb(exc_tb)
-                    cause = tb[len(tb)-1]
+    def process_exception(self, request, exception):
+        exc_info = sys.exc_info()
 
-                    filename = cause[0]
-                    line = cause[1]
-                    msg = traceback.format_exception_only(exc_type, exc_val)[0].replace('\n','; ')
+        exc_type, exc_val, exc_tb = exc_info
+        tb = traceback.extract_tb(exc_tb)
+        cause = tb[len(tb)-1]
 
-                    error = 'file=%s line=%s msg="%s"' % (filename, line, msg)
-                    context = {
-                        'traceback': traceback.format_tb(exc_tb)
-                    }
+        filename = cause[0]
+        line = cause[1]
+        msg = traceback.format_exception_only(exc_type, exc_val)[0].replace('\n','; ')
 
-                    self.log.error(error, {'context':context})
+        error = 'file=%s line=%s msg="%s"' % (filename, line, msg)
+        context = {
+            'traceback': traceback.format_tb(exc_tb),
+            'method': request.method,
+            'path': request.path,
+            'meta': request.META
+        }
 
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except:
-            self.handleError(record)
+        self.log.error(error, {'context':context})
