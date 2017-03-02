e1d245082a6955b683d3e86ff5fba669b48278d5
LomaxRx <andrewlomaxr@gmail.com>
2017-03-02 10:41:03 -0500

    check for start and end dates

--- event/models.py
+++ event/models.py
@@ -211,7 +211,10 @@ def set_events_date_query(user_date_query, tense):
     if user_date_query:
         if is_json(user_date_query):
             date_range = json.loads(user_date_query)
-            date_query = Q(date__range=(date_range['start'], date_range['end']))
+            if getattr(date_range, 'start', False) and getattr(date_range, 'end', False):
+                date_query = Q(date__range=(date_range['start'], date_range['end']))
+            else:
+                date_query = Q(id__isnull=True)
         else:
             date_query = Q(id__isnull=True)
     else:
