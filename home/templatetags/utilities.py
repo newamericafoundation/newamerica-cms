f7633fdb1926cbe7e1ab5a585076e19a8f4247f9
LomaxRx <andrewlomaxr@gmail.com>
2017-03-20 10:24:46 -0400

    dummy start_time

--- home/templatetags/utilities.py
+++ home/templatetags/utilities.py
@@ -149,9 +149,14 @@ def generate_dateline(post):
 # 	- multi-day events are considered past at the start time on the final day of the event (end_date + start_time)
 @register.filter
 def is_future(item):
-	start_time = item.start_time
+	if getattr(item, 'start_time', None):
+		start_time = item.start_time
+	else:
+		start_time = datetime.min.time()
+
 	start_date = item.date
 	end_date = item.end_date
+
 
 	if (end_date):
 		return is_datetime_future(start_time, end_date)
