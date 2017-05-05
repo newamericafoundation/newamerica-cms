91e267c31a83995a466fde307eab8c26a9191441
LomaxRx <andrewlomaxr@gmail.com>
2017-05-05 11:35:55 -0400

    limit page_size to 25

--- newamericadotorg/settings/base.py
+++ newamericadotorg/settings/base.py
@@ -163,5 +163,5 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50
+    'PAGE_SIZE': 25
 }
