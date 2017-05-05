e3bfc266d66016fa17b2f6574070559565a42aff
LomaxRx <andrewlomaxr@gmail.com>
2017-05-05 15:34:53 -0400

    jquery rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,29 @@
 import { rollup } from 'rollup';
 import babel from 'rollup-plugin-babel';
 import sass from 'rollup-plugin-sass';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs'
 
 export default {
   entry: 'newamericadotorg/assets/js/newamericadotorg.js',
   dest: 'newamericadotorg/static/js/newamericadotorg.js',
   format: 'iife',
-  moduleName: 'widgetail',
-  external: [
-    'react',
-    'react-dom',
-    'redux',
-    'react-redux',
-    'axios',
-    'redux-thunk'
-  ],
-  globals: {
-    'react': 'React',
-    'react-dom': 'ReactDOM',
-    'axios': 'axios',
-    'redux': 'Redux',
-    'react-redux': 'ReactRedux',
-    'redux-thunk': 'ReduxThunk'
-  },
+  moduleName: 'newamericadotorg',
+  globals: {},
   plugins: [
     sass({
-      output: 'build/widgetail.css',
+      output: 'newamericadotorg/static/css/newamericadotorg.css',
       insert: true
+    }),
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true
+    }),
+    commonjs({
+      namedExports: {
+          'node_modules/jquery/dist/jquery.min.js': [ 'jquery' ]
+      }
     }),
     babel()
   ]
