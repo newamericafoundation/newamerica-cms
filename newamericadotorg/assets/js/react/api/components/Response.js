2e1e5b5e4c033c6a560d6361425132d1dcaf62b2
LomaxRx <andrewlomaxr@gmail.com>
2017-06-02 12:18:49 -0400

    loading options for response

--- newamericadotorg/assets/js/react/api/components/Response.js
+++ newamericadotorg/assets/js/react/api/components/Response.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import LoadingIcon from '../../components/LoadingIcon';
 import { mapStateToProps, mapDispatchToProps } from './props';
 
 class Response extends Component {
@@ -9,20 +10,25 @@ class Response extends Component {
     component: PropTypes.oneOfType([
       PropTypes.func,
       PropTypes.string
-    ])
+    ]),
+    showLoading: PropTypes.bool,
+    transition: PropTypes.bool
   }
 
   static defaultProps = {
-    component: 'span'
+    component: 'div',
+    showLoading: false,
+    transition: false
   }
 
   render(){
-    let { children, className } = this.props;
+    let { children, className, transition, showLoading, component } = this.props;
     let { isFetching } = this.props.response;
     return (
       <this.props.component {...this.props}
-        className={'compose__response-component ' + (className||'') + (isFetching? ' is-fetching': '')}>
-        {children}
+        className={'compose__response-component ' + (transition ? ' fetch-transition ' : '') +  (className||'') + (isFetching? ' is-fetching': '')}>
+        {children || (component.props ? component.props.children : '')}
+        {(isFetching&&showLoading) && <div className="loading-icon-container"><LoadingIcon /></div>}
       </this.props.component>
     );
   }
