11bccc72e1b57bad1db5177a36999079ed02e13f
lomaxrx <andrewlomaxr@gmail.com>
2017-10-31 09:40:52 -0400

    type and spacing changes

--- newamericadotorg/assets/js/react/report/components/Authors.js
+++ newamericadotorg/assets/js/react/report/components/Authors.js
@@ -1,10 +1,10 @@
 const Authors = ({ authors }) => (
   <div className="report__authors">
-    <label className="block bold">Authors</label>
+    <label className="block bold bottom-margin">Authors</label>
     {authors.map((a)=>(
       <div className="report__authors__item">
         <label className="block">{`${a.first_name} ${a.last_name}`}</label>
-        <label className="block caption">{a.position}</label>
+        <label className="block caption top-margin">{a.position}</label>
       </div>
     ))}
   </div>
