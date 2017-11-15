766a727ac69c5b896479712e074de080ee6df3e4
lomaxrx <andrewlomaxr@gmail.com>
2017-11-15 11:54:08 -0500

    disable window scroll when interacting with contents menu

--- newamericadotorg/assets/js/react/report/components/ContentMenu.js
+++ newamericadotorg/assets/js/react/report/components/ContentMenu.js
@@ -1,13 +1,26 @@
 import { Link } from 'react-router-dom';
 import { Component } from 'react';
+import { connect } from 'react-redux';
 
 class ContentMenu extends Component {
-  state = { expanded: {} }
+  state = { expanded: {}, lastScrollPosition: 0 }
   constructor(props){
     super(props);
     props.report.sections.map((s)=>{
       this.state.expanded[s.title] = s.title == props.activeSection.title;
     });
+  }
+
+  disableScroll = () => {
+    this.state.lastScrollPosition = this.props.windowScrollPosition;
+    document.body.classList.add('scroll-disabled');
+    document.body.style.top = -this.state.lastScrollPosition + 'px';
+  }
+
+  enableScroll = () => {
+    document.body.classList.remove('scroll-disabled');
+    document.body.style.top = '';
+    window.scrollTo(0, this.state.lastScrollPosition);
   }
 
   toggleExpanded = (title) => {
@@ -26,7 +39,9 @@ class ContentMenu extends Component {
   render(){
     let { report: { url, sections }, open, closeMenu, activeSection } = this.props;
     return (
-      <div className="report__content-menu">
+      <div className="report__content-menu"
+      onMouseEnter={this.disableScroll} onTouchStart={this.disableScroll}
+      onMouseLeave={this.enableScroll} onTouchEnd={this.enableScroll}>
         {sections.map((s,i)=>(
           <span className={this.state.expanded[s.title] ? 'expanded' : ''}>
             <div className={`report__content-menu__item${activeSection.slug==s.slug? ' active' : ''}`}>
@@ -51,4 +66,9 @@ class ContentMenu extends Component {
     );
   }
 }
-export default ContentMenu;
+
+const mapStateToProps = (state) => ({
+  windowScrollPosition: state.site.scroll.position
+});
+
+export default connect(mapStateToProps)(ContentMenu);
