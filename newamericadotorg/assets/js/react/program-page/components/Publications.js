bdc0cf967da6674fcc545c2b97d05f2f582c8168
lomaxrx <andrewlomaxr@gmail.com>
2018-01-13 15:57:33 -0500

    pretty good shape UX for filter sidebar

--- newamericadotorg/assets/js/react/program-page/components/Publications.js
+++ newamericadotorg/assets/js/react/program-page/components/Publications.js
@@ -89,7 +89,7 @@ export default class Publications extends Component{
     let slug = location.pathname.match(/.+\/(.+)\/$/i)[1];
     let type = program.content_types.find((t)=>(t.slug === slug ));
     return (
-      <div className="program__publications row gutter-45 scroll-target" data-scroll-trigger-point="bottom" data-scroll-bottom-offset="115">
+      <div className="program__publications row gutter-45 scroll-target" data-scroll-trigger-point="bottom" data-scroll-bottom-offset="65">
         <div className="col-3 program__publications__filter-col">
           <Fetch component={Filters} name="programPage.publications"
             endpoint={'post'}
