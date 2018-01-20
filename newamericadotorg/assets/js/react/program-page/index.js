406067b6b242d3efbaa45451db93d5f2a3b040ee
lomaxrx <andrewlomaxr@gmail.com>
2018-01-20 14:21:49 -0500

    fix double fetch on publications filter

--- newamericadotorg/assets/js/react/program-page/index.js
+++ newamericadotorg/assets/js/react/program-page/index.js
@@ -35,6 +35,7 @@ class ProgramPage extends Component {
   render(){
     let { response: { results }, programType } = this.props;
     let root = programType == 'program' ? ':program' : ':program/:subprogram';
+    let contentSlugs = results.content_types.map((c)=>(c.slug)).join('|');
     return (
       <div className="container">
         <Router>
@@ -46,10 +47,7 @@ class ProgramPage extends Component {
             <Route path={`/${root}/our-people/`} render={(props)=>(<People programType={programType} {...props} program={results} /> )} />
             <Route path={`/${root}/events/`} render={(props)=>(<Events programType={programType} {...props} program={results} /> )} />
             <Route path={`/${root}/subprograms/`} render={(props)=>(<Subprograms {...props} program={results} /> )} />
-            <Route path={`/${root}/publications/`} render={(props)=>(<Publications programType={programType} {...props} program={results} /> )} />
-            {results.content_types.map((c,i)=>(
-              <Route path={`/${root}/${c.slug}/`} render={(props)=>(<Publications {...props} programType={programType} program={results} /> )} />
-            ))}
+            <Route path={`/${root}/(publications|${contentSlugs})/`} render={(props)=>(<Publications programType={programType} {...props} program={results} /> )} />
             {results.topics &&
               <Route path={`/${root}/topics/`} exact render={(props)=>(<TopicsList {...props} program={results} /> )} />}
             {this.topicRoutes(results.topics)}
