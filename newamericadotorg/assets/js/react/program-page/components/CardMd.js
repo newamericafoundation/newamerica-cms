3548e3b350583238f6c9b301626ea7547089b81b
LomaxRx <andrewlomaxr@gmail.com>
2018-02-07 20:29:14 -0500

    program story grid hack

--- newamericadotorg/assets/js/react/program-page/components/CardMd.js
+++ newamericadotorg/assets/js/react/program-page/components/CardMd.js
@@ -5,16 +5,20 @@ export default class CardMd extends Component {
   render(){
     let { post, image_size, loaded } = this.props;
     return (
-      <div className={`card md ${image_size || ''}`}>
-        <a href={post.url}>
-          <div className="card__image">
-            <Image thumbnail={post.story_image_thumbnail} image={post.story_image} loaded={loaded}/>
+      <div className={`card md ${image_size || ''} md-side`}>
+        <a href={post.url} className="row gutter-0">
+          <div className="col-6 col-md-12">
+            <div className="card__image">
+              <Image thumbnail={post.story_image_thumbnail} image={post.story_image} loaded={loaded}/>
+            </div>
           </div>
-          <div className="card__text">
-            <label className="card__text__title bold margin-top-0 block">{post.title}</label>
-            <label className="card__text__program caption margin-bottom-0 block">
-              {post.programs ? post.programs[0].name : ''} {post.content_type ? post.content_type.name : ''}
-            </label>
+          <div className="col-6 col-md-12">
+            <div className="card__text">
+              <label className="card__text__title bold margin-top-0 block">{post.title}</label>
+              <label className="card__text__program caption margin-bottom-0 block">
+                {post.programs ? post.programs[0].name : ''} {post.content_type ? post.content_type.name : ''}
+              </label>
+            </div>
           </div>
         </a>
       </div>
