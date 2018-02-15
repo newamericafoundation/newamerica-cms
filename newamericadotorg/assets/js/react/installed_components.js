767770ced67b4d429c6b1d10c9b5c35105ee2570
LomaxRx <andrewlomaxr@gmail.com>
2018-02-15 01:51:05 -0500

    remove indepth app

--- newamericadotorg/assets/js/react/installed_components.js
+++ newamericadotorg/assets/js/react/installed_components.js
@@ -4,7 +4,6 @@ export { default as publicationsHome } from './publications-home/index';
 export { default as eventsHome } from './events-home/index';
 export { default as weekly } from './weekly/index';
 export { default as search } from './search/index';
-export { default as inDepth } from './in-depth/index';
 export { default as topics } from './topics/index';
 export { default as mobileMenu } from './mobile-menu/index';
 export { default as programMobileMenu } from './program-mobile-menu/index';
