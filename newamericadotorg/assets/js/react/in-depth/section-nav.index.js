c7f1751605cc3736fdcec8d7934be0f54a0622bc
LomaxRx <andrewlomaxr@gmail.com>
2018-02-17 03:25:16 -0500

    in-depth menus

--- newamericadotorg/assets/js/react/in-depth/section-nav.index.js
+++ newamericadotorg/assets/js/react/in-depth/section-nav.index.js
@@ -1,15 +1,30 @@
 import { Component } from 'react';
 import { Slider } from '../components/Carousel';
+import { connect } from 'react-redux';
 
 const NAME = 'inDepthSectionNav';
 const ID = 'in-depth-section-nav';
 
 class APP extends Component {
-
+  goTo = (e) => {
+    window.location = e.target.value;
+  }
   render(){
-    let { sections, currentSlide } = this.props;
-    console.log(currentSlide);
+    let { sections, currentSlide, windowWidth } = this.props;
     sections = JSON.parse(sections);
+
+    if(windowWidth <= 768){
+      return (
+        <div className="in-depth__section__mobile-selector">
+          <select onChange={this.goTo} className="in-depth__section__mobile-selector__select">
+            {sections.map((s,i)=>(
+              <option key={`section-${i}`} selected={+currentSlide==i} value={s.url}>{s.title}</option>
+            ))}
+          </select>
+        </div>
+      );
+    }
+
     return (
       <div className="in-depth-section-nav">
           <Slider
@@ -17,7 +32,7 @@ class APP extends Component {
             slideToScroll={1}
             infinite={false}
             responsive={[
-              {breakpoint: 768, settings: {slidesToShow: 1}},
+              {breakpoint: 960, settings: {slidesToShow: 2}},
               {breakpoint: 100000, settings: {slidesToShow: 3}}
             ]}
             nextArrow={<div><label className="with-caret--right white"/></div>}
@@ -35,5 +50,11 @@ class APP extends Component {
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  windowWidth: state.site.windowWidth
+});
+
+APP = connect(mapStateToProps)(APP);
 
 export default { APP, ID, NAME };
