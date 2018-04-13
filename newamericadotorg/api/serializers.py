f21b7150d7ed3f4d59e2f1a03fa9dd4d5bee485f
LomaxRx <andrewlomaxr@gmail.com>
2018-04-13 16:17:44 -0400

    update topic serializer

--- newamericadotorg/api/serializers.py
+++ newamericadotorg/api/serializers.py
@@ -79,6 +79,8 @@ class TopicSerializer(ModelSerializer):
     subtopics = SerializerMethodField()
     description = SerializerMethodField()
     program = SerializerMethodField()
+    body = SerializerMethodField()
+    depth = SerializerMethodField()
 
     def get_subtopics(self, obj):
         return TopicSerializer(obj.get_children().live().specific(), many=True).data
@@ -86,14 +88,22 @@ class TopicSerializer(ModelSerializer):
     def get_description(self, obj):
         return obj.story_excerpt
 
+    def get_body(self, obj):
+        if not obj.body:
+            return None
+        return loader.get_template('components/post_body.html').render({ 'page': obj })
+
     def get_program(self, obj):
         if obj.parent_program:
             return obj.parent_program.id
 
+    def get_depth(self, obj):
+        return obj.depth - 5
+
     class Meta:
         model = IssueOrTopic
         fields = (
-            'id', 'url', 'title', 'slug', 'subtopics', 'description', 'program'
+            'id', 'url', 'title', 'slug', 'subtopics', 'description', 'program', 'body', 'depth'
         )
 
 
