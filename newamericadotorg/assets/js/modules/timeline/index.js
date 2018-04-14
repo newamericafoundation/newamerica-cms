1886e21a1e375685229396c8400890e0bf058e33
LomaxRx <andrewlomaxr@gmail.com>
2018-04-14 15:31:58 -0400

    weekly fix

--- newamericadotorg/assets/js/modules/timeline/index.js
+++ newamericadotorg/assets/js/modules/timeline/index.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 import { Timeline } from './timeline';
 window.$ = $;
 
-$('.timelineViz').forEach(function(){
+$('.timelineViz').each(function(){
     let data = JSON.parse(this.getAttribute('data-timeline-data'));
     let id = this.getAttribute('id');
     new Timeline(data, id);
