f8a0f5ed11706c2147f89acfe77d0c9cb2891b37
LomaxRx <andrewlomaxr@gmail.com>
2018-04-14 14:20:34 -0400

    put topics page back

--- issue/models.py
+++ issue/models.py
@@ -40,8 +40,7 @@ class IssueOrTopic(ProgramSimplePage):
     )
 
     def get_template(self, request):
-        return 'post_page.html'
-        #return 'programs/program.html'
+        return 'programs/program.html'
 
     def get_context(self, request):
         context = super(IssueOrTopic, self).get_context(request)
