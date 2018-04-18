869fd9530947721787bfad808000553bcf219aef
LomaxRx <andrewlomaxr@gmail.com>
2018-04-18 11:38:54 -0400

    pdf redirect

--- report/utils/views.py
+++ report/utils/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from report.models import Report
 from .pdf_gen import generate_pdf_response
 
@@ -10,7 +10,10 @@ def redirect_report_section(request, **kwargs):
 def pdf(request, **kwargs):
     path = request.path.split('/')[:-2]
     report = Report.objects.filter(slug=path[len(path)-1]).first()
-    return generate_pdf_response(report, request)
+    if getattr(request, 'live', None) or not report.report_pdf:
+        return generate_pdf_response(report, request)
+    return redirect(report.report_pdf.file.url)
+
 
 def pdf_html(request, **kwargs):
     path = request.path.split('/')[:-3]
