97c03a9326c34bb71b6483d020549c6019e99dbe
LomaxRx <andrewlomaxr@gmail.com>
2018-04-18 02:08:25 -0400

    minify endnote

--- newamericadotorg/assets/js/react/report/components/EndnoteAside.js
+++ newamericadotorg/assets/js/react/report/components/EndnoteAside.js
@@ -2,7 +2,19 @@ import { Component } from 'react';
 import { PlusX } from '../../components/Icons';
 
 class Endnote extends Component {
+  cleanEndnote = () => {
+    let { endnote : { note } } = this.props;
+    let r = /(http.+?)(;| |$|<)/g
+    let matches = note.match(r);
+    if(matches){
+      for(let m of matches){
+        m = m.replace(';', '').replace('<', '');
+        note = note.replace(m, `<a href="${m}" target="_blank">source</a>`);
+      }
+    }
 
+    return note;
+  }
   render(){
     let { endnote, top } = this.props;
 
@@ -13,7 +25,7 @@ class Endnote extends Component {
         </div>
         {endnote &&<span>
           <label className="inline" >{`${endnote.number} `}</label>
-          <label className="inline" dangerouslySetInnerHTML={{__html: endnote.note}}></label>
+          <label className="inline" dangerouslySetInnerHTML={{__html: this.cleanEndnote()}}></label>
         </span>}
       </div>
     );
