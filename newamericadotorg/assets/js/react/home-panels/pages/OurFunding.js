fefac833702bf758a8fdff73186df56af9c284c6
LomaxRx <andrewlomaxr@gmail.com>
2018-07-23 16:18:45 -0400

    charity navigator update

--- newamericadotorg/assets/js/react/home-panels/pages/OurFunding.js
+++ newamericadotorg/assets/js/react/home-panels/pages/OurFunding.js
@@ -222,7 +222,7 @@ class OurFundingMain extends Component {
         <Body data={how_were_funded} aside={<div className="buttons">
           <div className="charity-navigator-img margin-bottom-25">
             {/* <img src={charity_navigator.inline_image[0].url}/> */}
-            <img src="https://na-production.s3.amazonaws.com/images/4Star_234x60bw.min-800x800.jpg" style={{width: '100%', maxWidth: '350px', margin: '0 auto', display: 'block'}}/>
+            <img src="https://newamericadotorg.s3.amazonaws.com/images/logos.2e16d0ba.fill-800x550.png" style={{width: '100%', maxWidth: '350px', margin: '0 auto', display: 'block'}}/>
           </div>
           <div style={{textAlign: "center"}}>
             <a className="button" href={donate_button.button[0].button_link}>{donate_button.button[0].button_text}</a>
