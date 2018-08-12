e9dfa9c3d98cf785ed3cf6611bb665239177b131
LomaxRx <andrewlomaxr@gmail.com>
2018-08-12 15:08:08 -0400

    static_url fix

--- custom_storages.py
+++ custom_storages.py
@@ -5,7 +5,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
     bucket_name = settings.STATIC_BUCKET_NAME
-    custom_domain = settings.CLOUDFRONT_STATIC_URL
+    custom_domain = settings.CLOUDFRONT_STATIC_URL.replace('https://', '')
 
 class MediaStorage(S3Boto3Storage):
     bucket_name = settings.AWS_STORAGE_BUCKET_NAME
