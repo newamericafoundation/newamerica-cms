4fb6f34df2f5fca6346d0b55983da33c5239dfce
LomaxRx <andrewlomaxr@gmail.com>
2018-09-10 12:57:41 -0400

    english learners redirect

--- newamericadotorg/redirects.py
+++ newamericadotorg/redirects.py
@@ -4,3 +4,9 @@ def future_property_rights(request, **kwargs):
     path = request.path.split('/')[3:]
     url = '/future-property-rights/' + '/'.join(path)
     return redirect(url)
+
+def dual_language_learners(request, **kwargs):
+    path = request.path.split('/')
+    path[2] = 'english-learners'
+    url = '/'.join(path)
+    return redirect(url)
