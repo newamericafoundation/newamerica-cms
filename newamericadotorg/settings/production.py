5b00aaccadb16292bdc11ea8c0d3999e18c978e3
Loren Riesenfeld <lorenries@users.noreply.github.com>
2018-11-20 15:34:37 -0500

    downgrade elasticsearch 🤦

--- newamericadotorg/settings/production.py
+++ newamericadotorg/settings/production.py
@@ -59,7 +59,7 @@ es_url = os.getenv('SEARCHBOX_URL', "http://localhost:9200/")
 
 WAGTAILSEARCH_BACKENDS = {
     'default': {
-        'BACKEND': 'wagtail.search.backends.elasticsearch6',
+        'BACKEND': 'wagtail.search.backends.elasticsearch2',
         'URLS': [es_url],
         'INDEX': 'elasticsearch',
         'TIMEOUT': 1500,
