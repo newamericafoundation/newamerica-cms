563f51ef537534b8c712a283742d22bdd1c6f110
lorenries <lorenries@users.noreply.github.com>
2019-02-19 16:38:39 -0500

    upgrade webpack dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,102 +1,134 @@
-const path = require("path");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const webpack = require("webpack");
-const WebpackBabelExternalsPlugin = require('webpack-babel-external-helpers-2');
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
-module.exports = {
-  mode: NODE_ENV,
-  entry: {
-		"newamericadotorg": "./newamericadotorg/assets/newamericadotorg.js"
-	},
-  output: {
-		filename: NODE_ENV == "development" ? "static/js/[name].js" : "static/js/[name]-[hash].js",
-    chunkFilename: NODE_ENV == "development" ? "static/js/[name].js" : "static/js/[name]-[hash].js",
-    publicPath: `${process.env.STATIC_URL || ''}/`,
-    path: path.resolve(__dirname, "./newamericadotorg"),
-    crossOriginLoading: "anonymous"
-  },
-  devtool: 'cheap-module-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.scss/,
-        use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-
-					},
-					'css-loader',
-					{
-						loader: 'sass-loader',
-						options: {
-							data: `$static: ${NODE_ENV==='development' ? '"/static"' : `"${process.env.STATIC_URL}/static"` };`
-						}
-					},
-          {
-            loader: 'sass-resources-loader',
-            options: {
-              resources: "./newamericadotorg/assets/scss/_mixins.scss"
+module.exports = env => {
+  return {
+    mode: NODE_ENV,
+    entry: {
+      newamericadotorg: './newamericadotorg/assets/newamericadotorg.js',
+      polyfills: './newamericadotorg/assets/polyfills.js'
+    },
+    output: {
+      filename:
+        NODE_ENV === 'development'
+          ? 'static/js/[name].js'
+          : 'static/js/[name]-[hash].js',
+      chunkFilename:
+        NODE_ENV === 'development'
+          ? 'static/js/[name].js'
+          : 'static/js/[name]-[hash].js',
+      publicPath: `${process.env.STATIC_URL || ''}/`,
+      path: path.resolve(__dirname, './newamericadotorg'),
+      crossOriginLoading: 'anonymous'
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.scss/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                data: `$static: ${
+                  NODE_ENV === 'development'
+                    ? '"/static"'
+                    : `"${process.env.STATIC_URL}/static"`
+                };`
+              }
+            },
+            {
+              loader: 'sass-resources-loader',
+              options: {
+                resources: './newamericadotorg/assets/scss/_mixins.scss'
+              }
             }
+          ]
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loaders: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: '> 1%, last 2 versions, Firefox ESR',
+                  modules: false,
+                  useBuiltIns: 'entry'
+                }
+              ],
+              '@babel/preset-react'
+            ],
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-proposal-object-rest-spread',
+              '@babel/plugin-syntax-dynamic-import'
+            ]
           }
-				]
-      },
-      // {
-      //   test: /\.js$/,
-      //   exclude: /node_modules/,
-      //   loaders: "eslint-loader",
-      // },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: "babel-loader",
-        options: {
-          presets: ["env", "react", "stage-0", "es2015"],
-          plugins: ["transform-class-properties", "transform-object-rest-spread"]
         }
+      ]
+    },
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          parallel: true,
+          sourceMap: true
+        }),
+        new OptimizeCSSAssetsPlugin({})
+      ],
+      splitChunks: {
+        chunks(chunk) {
+          return chunk.name !== 'polyfills';
+        },
+        maxInitialRequests: 6
       }
-    ]
-  },
-	optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename:
+          NODE_ENV === 'development'
+            ? 'static/css/[name].css'
+            : 'static/css/[name]-[hash].css',
+        chunkFilename:
+          NODE_ENV === 'development'
+            ? 'static/css/[name].css'
+            : 'static/css/[name]-[hash].css'
       }),
-      new OptimizeCSSAssetsPlugin({})
-    ],
-		splitChunks: {
-      chunks: 'all',
-      minSize: 10000,
-			cacheGroups: {
-        default: false,
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          enforce: true,
-          chunks: 'all'
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'templates/base.html',
+        staticUrl: `${process.env.STATIC_URL || ''}/static`,
+        template: 'newamericadotorg/templates/index.html',
+        inject: false,
+        serviceWorker: '/static/js/sw.js'
+      }),
+      new GenerateSW({
+        swDest: 'static/js/sw.js',
+        importWorkboxFrom: 'local',
+        include: [/\.css$/, /\.js$/, /\.svg$/],
+        importsDirectory: 'static/workbox-assets',
+        modifyUrlPrefix: {
+          '/static': `${process.env.STATIC_URL || ''}/static`
         }
-			}
-		}
-  },
-  plugins: [
-		new MiniCssExtractPlugin({
-      filename: "templates/style.css",
-      chunkFilename: NODE_ENV == "development" ? "static/css/[name].css" : "static/css/[name]-[hash].css"
-    }),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(NODE_ENV)
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'templates/base.html',
-      staticUrl: `${process.env.STATIC_URL || ''}/static`,
-      template: 'newamericadotorg/templates/index.html',
-      inject: false
-    })
-  ]
+      }),
+      NODE_ENV === 'development' && new BundleAnalyzerPlugin()
+    ].filter(plugin => plugin)
+  };
 };
--- webpack.config.lite.js
+++ webpack.config.lite.js
@@ -1,73 +1,77 @@
-const path = require("path");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const webpack = require("webpack");
-const WebpackBabelExternalsPlugin = require('webpack-babel-external-helpers-2');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const webpack = require('webpack');
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
   mode: NODE_ENV,
   entry: {
-		"newamericadotorg.lite": "./newamericadotorg/assets/newamericadotorg.lite.js"
-	},
+    'newamericadotorg.lite':
+      './newamericadotorg/assets/newamericadotorg.lite.js'
+  },
   output: {
-		filename: "static/js/[name].js",
-    path: path.resolve(__dirname, "./newamericadotorg"),
+    filename: 'static/js/[name].js',
+    path: path.resolve(__dirname, './newamericadotorg')
   },
   module: {
     rules: [
       {
         test: /\.scss/,
         use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-
-					},
-					'css-loader',
-					{
-						loader: 'sass-loader',
-						options: {
-							data: '$static: "https://d3fvh0lm0eshry.cloudfront.net/static";'
-						}
-					},
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              data: '$static: "https://d3fvh0lm0eshry.cloudfront.net/static";'
+            }
+          },
           {
             loader: 'sass-resources-loader',
             options: {
-              resources: "./newamericadotorg/assets/scss/_mixins.scss"
+              resources: './newamericadotorg/assets/scss/_mixins.scss'
             }
           }
-				]
+        ]
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loaders: "babel-loader",
+        loaders: 'babel-loader',
         options: {
-          presets: ["env", "react", "stage-0", "es2015"],
-          plugins: ["transform-class-properties", "transform-object-rest-spread"]
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: '> 1%, last 2 versions, Firefox ESR',
+                modules: false,
+                useBuiltIns: 'entry'
+              }
+            ],
+            '@babel/preset-react'
+          ],
+          plugins: [
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-proposal-object-rest-spread',
+            '@babel/plugin-syntax-dynamic-import'
+          ]
         }
       }
     ]
   },
   optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true
-      }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
+    minimizer: [new OptimizeCSSAssetsPlugin({})]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "static/css/[name].css"
+      filename: 'static/css/[name].css'
     }),
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(NODE_ENV)
+      'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
     })
   ]
 };
