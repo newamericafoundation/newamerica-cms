1fdf1146995becef4efc6ff418a16c3edf05e6af
lorenries <lorenries@users.noreply.github.com>
2019-03-01 16:23:32 -0500

    Remove opacity transition on scroll enter to allow component lazy-loading

--- newamericadotorg/assets/react/home-panels/components/ImageAside.js
+++ newamericadotorg/assets/react/home-panels/components/ImageAside.js
@@ -4,18 +4,23 @@ import Panel from './Panel';
 import React from 'react';
 
 export default class ImageAside extends Panel {
-  render(){
+  render() {
     let { data, grayscale } = this.props;
     return (
-      <section className="home__panel__image-aside padding-110 scroll-target" data-scroll-trigger-point="bottom" data-scroll-offset="-10vh">
+      <section className="home__panel__image-aside padding-110">
         <div className="container--1080">
           <div className="row gutter-20">
             <div className="col-md-6 home__panel__image-aside__image margin-bottom-35">
-              <img src={data.inline_image[0].url} className={`${grayscale ? 'grayscale' : ''}`} />
+              <img
+                src={data.inline_image[0].url}
+                className={`${grayscale ? 'grayscale' : ''}`}
+              />
             </div>
-            <div className="col-md-5 push-md-1 home__panel__image-aside__text" >
+            <div className="col-md-5 push-md-1 home__panel__image-aside__text">
               <div className="home__panel__image-aside__text__wrapper">
-                <h1 className="margin-top-0 margin-bottom-25">{data.heading[0]}</h1>
+                <h1 className="margin-top-0 margin-bottom-25">
+                  {data.heading[0]}
+                </h1>
                 <p className="margin-bottom-0 margin-top-25">
                   {this.parseHTMLText(data.introduction[0])}
                 </p>
