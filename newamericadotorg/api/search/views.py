c8aad928a3b7eee7f7b4cda11ef5dfb5783cee2c
Karl Hobley <karl@kaed.uk>
2019-11-01 14:35:54 +0000

    Disable partial_match

--- newamericadotorg/api/search/views.py
+++ newamericadotorg/api/search/views.py
@@ -13,7 +13,7 @@ class SearchList(ListAPIView):
 
     def get_queryset(self):
         search = self.request.query_params.get('query', None)
-        results = Page.objects.live().search(search)
+        results = Page.objects.live().search(search, partial_match=False)
         query = Query.get(search)
         query.add_hit()
 
