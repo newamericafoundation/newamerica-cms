efc0d2e8101a932cc124f0bba90306c17c395138
Karl Hobley <karl@kaed.uk>
2019-11-01 15:02:20 +0000

    Use faster method for excluding private pages from search results

--- newamericadotorg/api/search/views.py
+++ newamericadotorg/api/search/views.py
@@ -7,30 +7,33 @@ from wagtail.search.models import Query
 
 from .serializers import SearchSerializer
 
+
+def exclude_invisible_pages(request, pages):
+    # Get list of pages that are restricted to this user
+    restricted_pages = [
+        restriction.page
+        for restriction in PageViewRestriction.objects.all().select_related('page')
+        if not restriction.accept_request(request)
+    ]
+
+    # Exclude the restricted pages and their descendants from the queryset
+    for restricted_page in restricted_pages:
+        pages = pages.not_descendant_of(restricted_page, inclusive=True)
+
+    return pages
+
+
 class SearchList(ListAPIView):
     serializer_class = SearchSerializer
     filter_backends = (DjangoFilterBackend,SearchFilter)
 
     def get_queryset(self):
         search = self.request.query_params.get('query', None)
-        results = Page.objects.live()
+        results = exclude_invisible_pages(self.request, Page.objects.live())
 
         if search:
             results = results.search(search, partial_match=False)
             query = Query.get(search)
             query.add_hit()
 
-        # search queryset does not allow .public(). manually exclude restricted pages
-        public_results =[]
-        restrictions = PageViewRestriction.objects.all()
-        for obj in results:
-            private = False
-            for restriction in restrictions:
-                if obj.id == restriction.page.id or obj.is_descendant_of(restriction.page):
-                    private = True
-                    break
-
-            if not private:
-                public_results.append(obj)
-
-        return public_results
+        return results
