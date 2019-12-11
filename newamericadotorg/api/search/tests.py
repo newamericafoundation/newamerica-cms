c862311836acd7ab0493c5c4d4eb3e12904a9ced
Karl Hobley <karl@torchbox.com>
2019-12-11 17:14:56 +0000

    Add a few tests

--- newamericadotorg/api/search/tests.py
+++ newamericadotorg/api/search/tests.py
@@ -1,6 +1,9 @@
-from rest_framework.test import APITestCase
+from io import StringIO
 
+from django.core import management
+from rest_framework.test import APITestCase, APIClient
 from wagtail.core.models import PageViewRestriction
+from wagtail.search.backends import get_search_backend
 
 from test_factories import PostFactory
 
@@ -13,28 +16,55 @@ class SearchAPITests(APITestCase):
 
         program = PostFactory.create_program(home_page=home_page)
 
-        posts = PostFactory.create_program_content(50,
+        cls.posts = PostFactory.create_program_content(50,
             program=program,
             content_page_type=ProgramBlogPostsPage,
             post_type=BlogPost,
             post_data={'title': 'Unique Query'}
         )
 
-        cls.posts = posts
+        # Some posts that shouldn't be returned in results
+        PostFactory.create_program_content(10,
+            program=program,
+            content_page_type=ProgramBlogPostsPage,
+            post_type=BlogPost,
+            post_data={'title': 'Foo'}
+        )
+
+    def setUp(self):
+        management.call_command('update_index', stdout=StringIO(), chunk_size=50)
 
     def test_search_all(self):
         url = '/api/search/?query=unique%20query'
         result = self.client.get(url).json()
 
-        self.assertEquals(getattr(result, 'error', False), False)
+        self.assertNotIn('error', result)
         self.assertEquals(result['count'], 50)
+
+    def test_partial_match_disabled(self):
+        url = '/api/search/?query=uniq'
+        result = self.client.get(url).json()
+
+        self.assertNotIn('error', result)
+        self.assertEquals(result['count'], 0)
 
     def test_restricted(self):
         for p in self.posts[:25]:
-            PageViewRestriction(page=p).save()
+            last_restriction = PageViewRestriction.objects.create(page=p, restriction_type=PageViewRestriction.PASSWORD)
 
         url = '/api/search/?query=unique%20query'
         result = self.client.get(url).json()
 
-        self.assertEquals(getattr(result, 'error', False), False)
+        self.assertNotIn('error', result)
         self.assertEquals(result['count'], 25)
+
+        # Now check that the page appears in results when the restriction is passed
+        session = self.client.session
+        session[PageViewRestriction.passed_view_restrictions_session_key] = [last_restriction.id]
+        session.save()
+
+        url = '/api/search/?query=unique%20query'
+        result = self.client.get(url).json()
+
+        self.assertNotIn('error', result)
+        self.assertEquals(result['count'], 26)
