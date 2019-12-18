7b3864ffa134045aaed0b6d64416c11a5b5906a1
nmorduch <1051450+nmorduch@users.noreply.github.com>
2019-12-18 14:48:19 -0500

    Test that articles and in depth articles get the correct ordered_date_string

--- in_depth/tests.py
+++ in_depth/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from datetime import date
 
-# Create your tests here.
+from wagtail.tests.utils import WagtailPageTests
+from wagtail.core.models import Page
+
+from test_factories import PostFactory
+
+from home.models import HomePage, CustomImage, PostAuthorRelationship
+from in_depth.models import InDepthProject, AllInDepthHomePage
+
+class InDepthTests(WagtailPageTests):
+    def setUp(self):
+        self.login()
+
+        self.home_page = PostFactory.create_homepage()
+
+        self.all_in_depth_home = self.home_page.add_child(
+            instance=AllInDepthHomePage(title='In Depth Home Page')
+        )
+
+        self.indepthproject = self.all_in_depth_home.add_child(instance=InDepthProject(
+            title="In Depth Project",
+            date=str(date.today())
+        ))
+        self.indepthproject.save()
+
+    def test_indepthproject_has_correct_ordered_date_string(self):
+        self.assertEqual(
+            self.indepthproject.ordered_date_string, f'{str(self.indepthproject.date)}-{self.indepthproject.id}'
+        )
+
