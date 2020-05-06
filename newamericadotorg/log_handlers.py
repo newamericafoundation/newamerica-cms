d980735f3bd7879f42abbb4597ed86f8317f2366
Karl Hobley <karl@torchbox.com>
2020-05-06 13:03:31 +0100

    Remove APIExceptionMiddleware
    This allows Sentry to log the error properly. The code that calls the
    API (`fetchData` in `actions.js`) will already handle these errors
    properly.

--- newamericadotorg/log_handlers.py
+++ newamericadotorg/log_handlers.py
@@ -1,8 +1,4 @@
-import logging, traceback, os, sys, json
-from newamericadotorg.helpers import is_json
-from logdna import LogDNAHandler as LogDNA
-from django.http import JsonResponse
-from django.conf import settings
+import logging
 
 
 class LoggerFilter(logging.Filter):
@@ -11,37 +7,4 @@ class LoggerFilter(logging.Filter):
 
 
 logger = logging.getLogger('weasyprint')
-logger.addFilter(LoggerFilter())
-
-
-class APIExceptionMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        return response
-
-    def process_exception(self, request, exception):
-        if 'api' in request.path and not settings.DEBUG:
-            exc_info = sys.exc_info()
-
-            exc_type, exc_val, exc_tb = exc_info
-            tb = traceback.extract_tb(exc_tb)
-            cause = tb[len(tb)-1]
-
-            filename = cause[0]
-            line = cause[1]
-            msg = traceback.format_exception_only(exc_type, exc_val)[0].replace('\n','; ')
-
-            error_source = 'file=%s line=%s' % (filename, line)
-            return JsonResponse({
-                'count': None,
-                'next': None,
-                'previous': None,
-                'results': [],
-                'error': True,
-                'error_source': error_source,
-                'message': msg
-            })
-
+    .addFilter(LoggerFilter())
