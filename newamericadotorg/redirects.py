7fbe11c03ba714713a9e51c7672c591807c93e06
nmorduch <1051450+nmorduch@users.noreply.github.com>
2020-08-18 11:30:31 -0400

    Add redirect for New Practice Lab

--- newamericadotorg/redirects.py
+++ newamericadotorg/redirects.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, redirect
 
+
+# Subprograms changing to programs
+
 def future_property_rights(request, **kwargs):
     path = request.path.split('/')[3:]
     url = '/future-property-rights/' + '/'.join(path)
     return redirect(url)
+
+def new_practice_lab(request, **kwargs):
+    path = request.path.split('/')[3:]
+    url = '/new-practice-lab/' + '/'.join(path)
+    return redirect(url)
+
+
+# Name changes
 
 def dual_language_learners(request, **kwargs):
     path = request.path.split('/')
