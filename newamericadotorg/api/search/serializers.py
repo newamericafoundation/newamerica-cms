479b410d13c65611f11f9f5b44c411e5cb1479ad
jacobtoppm <jacobtm@torchbox.com>
2020-09-29 12:56:16 +0100

    Try and fetch story image regardless of Page type. Try to get program from parent_programs, but otherwise, find ancestor programs in order to accommodate pages linked to Programs via tree structure, and prevent program not being displayed in search

--- newamericadotorg/api/search/serializers.py
+++ newamericadotorg/api/search/serializers.py
@@ -4,7 +4,7 @@ from wagtail.core.models import Page
 from wagtail.images.views.serve import generate_image_url
 
 from person.models import Person
-from home.models import Post
+from home.models import Post, Program
 from event.models import Event
 
 from newamericadotorg.api.helpers import get_content_type
@@ -23,13 +23,18 @@ class SearchSerializer(ModelSerializer):
             data['last_name'] = obj.last_name
             data['position'] = obj.position_at_new_america
         elif isinstance(obj, Post) or type(obj) == Event:
-            data['story_image'] = generate_image_url(obj.story_image, 'max-300x240') if obj.story_image else None
             data['date'] = obj.date
-            data['programs'] = PostProgramSerializer(obj.parent_programs, many=True).data
 
         if isinstance(obj, Post):
             data['authors'] = AuthorSerializer(obj.post_author, many=True, context=self.context).data
+        
+        data['story_image'] = generate_image_url(obj.story_image, 'max-300x240') if getattr(obj, 'story_image', None) else None
 
+        if hasattr(obj, 'parent_programs'):
+            data['programs'] = PostProgramSerializer(obj.parent_programs, many=True).data
+        else:
+            programs = Program.objects.ancestor_of(obj).order_by('-depth')
+            data['programs'] = PostProgramSerializer(programs, many=True).data
 
         data['content_type'] = get_content_type(obj)
         return data
