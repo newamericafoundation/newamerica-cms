af5d93947647ac2b7c11b87ad0f86a3ef8be5166
IMGoodrich <igoodrich@phase2technology.com>
2020-12-15 09:20:53 -0800

    fix: Manualy cleaning/reverting branch to initial state. Leaving autocomplete.

--- newamericadotorg/api/urls.py
+++ newamericadotorg/api/urls.py
@@ -47,5 +47,4 @@ api_urls = [
     url(r'^content-types/$', meta_views.ContentList.as_view()),
     url(r'^subscribe/$', subscribe_views.subscribe),
     url(r'^jobs/$', jobs_views.JobsList.as_view()),
-    url(r'^survey/$', survey_views.SurveyDetail.as_view()),
 ]
