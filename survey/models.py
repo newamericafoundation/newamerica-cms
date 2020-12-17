48d568b95c69955a953801de7a59e3eb83a49baa
IMGoodrich <igoodrich@phase2technology.com>
2020-12-17 14:37:24 -0800

    chore: Remove autocomplete_create func from survey index types. Add Utils file. Generate IndexPage on creation. Cleanup.

--- survey/models.py
+++ survey/models.py
@@ -22,20 +22,16 @@ from wagtail.snippets.models import register_snippet
 from programs.models import AbstractContentPage
 from wagtailautocomplete.edit_handlers import AutocompletePanel
 
-
+from multiselectfield import MultiSelectField
+from .utils import MONTH_CHOICES, DATA_TYPE_CHOICES
 class ProgramSurveysPage(AbstractContentPage):
-    """
-    A page which inherits from the abstract Page model and
-    returns all Articles associated with a specific Program
-    or Subprogram
-    """
-
     parent_page_types = ['programs.Program', 'programs.Subprogram', 'programs.Project']
     subpage_types = ['Survey', 'Commentary', 'SurveyValuesIndex']
 
-    @property
-    def content_model(self):
-        return Survey
+    def save(self, *args, **kwargs):
+      super(ProgramSurveysPage, self).save(*args, **kwargs)
+      if self.get_children_count() == 0:
+        self.add_child(instance=SurveyValuesIndex(title=self.title + " Values Index", slug=slugify(self.title + " Values Index")))
 
     class Meta:
         verbose_name = "Surveys Homepage"
@@ -44,89 +40,32 @@ class ProgramSurveysPage(AbstractContentPage):
         return self.title
 
 class SurveyOrganization(Page):
-    parent_page_types = ['SurveyValuesIndex', 'ProgramSurveysPage']
-    subpage_type = []
-    
-    @classmethod
-    def autocomplete_create(cls: type, value: str):
-      test = ProgramSurveysPage.objects.first().title
-    
-      orgs_index_page = SurveyValuesIndex.objects.first()
-      title = value
-
-      new = cls(title=value)
-      orgs_index_page.add_child(instance=new)
-      orgs_index_page.save()
-      return new
-
-    def clean(self):
-      """Override the values of title and slug before saving."""
-      super().clean()
-      if not self.slug:
-        self.slug = slugify(self.title)   
-  
+    parent_page_types = ['SurveyValuesIndex']    
     
     def __str__(self):
       return self.title
     class Meta:
         verbose_name_plural = 'Survey Organizations'
 
-
-
 class DemographicKey(Page):
-    parent_page_types = ['SurveyValuesIndex', 'ProgramSurveysPage']
-    subpage_type = []
-
-    @classmethod
-    def autocomplete_create(cls: type, value: str):
-      test = ProgramSurveysPage.objects.all()
-      orgs_index_page = SurveyValuesIndex.objects.first()
-      title = value
-      new = cls(title=value)
-      orgs_index_page.add_child(instance=new)
-      orgs_index_page.save()
-      return new
-
-
-    def clean(self):
-      """Override the values of title and slug before saving."""
-      super().clean()
-      # self.title = '%s %s' % (self.lable)
-      if not self.slug:
-        self.slug = slugify(self.title)   
-  
+    parent_page_types = ['SurveyValuesIndex']
     
     def __str__(self):
       return self.title
     class Meta:
         verbose_name_plural = 'Demographic Keys'
 
+class SurveyTags(Page):
+    parent_page_types = ['SurveyValuesIndex']
+    
+    def __str__(self):
+      return self.title
+    class Meta:
+        verbose_name_plural = 'Survey Tags'
+
 class SurveyValuesIndex(Page):
-    """
-    A page which inherits from the abstract Page model and
-    returns all Articles associated with a specific Program
-    or Subprogram
-    """
-
     parent_page_types = ['ProgramSurveysPage']
-    subpage_type = ['SurveyOrganization', 'DemographicKey', "Survey"]
-
-    def get_orgs(self):
-      return SurveyOrganization.objects.live().descendant_of(self)
-  
-    def get_context(self, request, *args, **kwargs):
-      context = super(SurveyValuesIndex, self).get_context(request)
-
-      orgs = self.get_orgs() 
-
-      context['orgs'] = orgs
-
-      return context
-
-    @property
-    def content_model(self):
-        return SurveyOrganization
-
+    subpage_type = ['SurveyOrganization', 'DemographicKey', 'SurveyTags']
     class Meta:
         verbose_name = "Surveyindex Homepage"
 
@@ -134,30 +73,11 @@ class SurveyValuesIndex(Page):
         return self.title
 
 
-class Survey(Post, RoutablePageMixin):
+class Survey(Post):
     template = 'survey/survey.html'
-    
-    MONTH_CHOICES = (
-      (0, 'N/A'),
-      (1, 'January'),
-      (2, 'February'),
-      (3, 'March'),
-      (4, 'April'),
-      (5, 'May'),
-      (6, 'June'),
-      (7, 'July'),
-      (8, 'August'),
-      (9, 'September'),
-      (10, 'October'),
-      (11, 'November'),
-      (12, 'December')
-    )
-    parent_page_types = ['ProgramSurveysPage', 'SurveyValuesIndex']
-    subpage_type=[]
-    # current_year = datetime.datetime.now().year
-    # print(current_year)
+ 
+    parent_page_types = ['ProgramSurveysPage']
 
-    study_title= models.CharField(max_length=250, blank=True, null=True)
     org = ParentalManyToManyField('SurveyOrganization', related_name='SurveyOrganization', blank=True)
     year = models.IntegerField(help_text='Year Survey was condicted.', blank=True, default=2000)
     month = models.IntegerField(choices=MONTH_CHOICES, default=None, help_text='Month Survey was condicted, if applicable.')
@@ -165,21 +85,25 @@ class Survey(Post, RoutablePageMixin):
     sample_demos = models.CharField(max_length=250, blank=True, null=True, help_text='Text displayed on the dashboard')
     demos_key = ParentalManyToManyField('DemographicKey', help_text='Indexable demographic groups', blank=True, default=False)
     findings = RichTextField(blank=True, null=True, max_length=12500)
+    data_type = MultiSelectField(choices=DATA_TYPE_CHOICES, default=['QUANT', 'QUAL'])
+    national = models.BooleanField(default=True)
     link = models.URLField(blank=True, null=True)
     file = models.FileField(blank=True, null=True)
     assoc_commentary = ParentalManyToManyField('Commentary', blank=True, through='Commented_Survey', related_name='surveys')
+    
     content_panels = [
       MultiFieldPanel([
         FieldPanel('date'),
       ], heading='Survey Created'),
       MultiFieldPanel([
-        FieldPanel('title'),
         AutocompletePanel('org'),
         FieldPanel('year'),
         FieldPanel('month'),
         FieldPanel('sample_number'),
         FieldPanel('sample_demos'),
         AutocompletePanel('demos_key'),
+        FieldPanel('data_type', widget=forms.CheckboxSelectMultiple),
+        FieldPanel('national'),
         FieldPanel('findings'),
         AutocompletePanel('assoc_commentary')
       ], heading='Survey Data'),
@@ -194,7 +118,6 @@ class Commentary(Post, RoutablePageMixin):
   template = 'survey/commentary.html'
 
   parent_page_types = ['ProgramSurveysPage']
-  subpage_types = []
 
   assoc_surveys = ParentalManyToManyField('Survey', through='Commented_Survey', blank=True, related_name='commentaries')
 
