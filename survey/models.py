465764e95d96d44f21fc9eb0edc955375375f72f
IMGoodrich <igoodrich@phase2technology.com>
2020-12-21 15:39:24 -0800

    chore: Rename ProgramSurveysPage to SurveyHomePage. Add authors inline panel to SurveyHomepage. Cleanup.

--- survey/models.py
+++ survey/models.py
@@ -1,35 +1,43 @@
 import json
 from home.models import Post
-import datetime
+from person.models import Person
+from programs.models import AbstractContentPage, Project
 
 from django import forms
 from django.db import models
 from django.utils.text import slugify
-
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
-from modelcluster.contrib.taggit import ClusterTaggableManager
-from modelcluster.models import ClusterableModel
 
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, StreamFieldPanel
-from wagtail.images.edit_handlers import ImageChooserPanel
-from wagtail.contrib.routable_page.models import RoutablePageMixin, route
-from wagtail.core.models import Page, Orderable
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, StreamFieldPanel, PageChooserPanel
+from wagtail.contrib.routable_page.models import RoutablePageMixin
 from wagtail.core.fields import RichTextField, StreamField
-from wagtail.documents.blocks import DocumentChooserBlock
+from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
-from wagtail.snippets.models import register_snippet
 
-from programs.models import AbstractContentPage
+from multiselectfield import MultiSelectField
 from wagtailautocomplete.edit_handlers import AutocompletePanel
 
-from programs.models import Project
-from multiselectfield import MultiSelectField
 from .utils import MONTH_CHOICES, DATA_TYPE_CHOICES
 
-class ProgramSurveysPage(AbstractContentPage):
+class PageAuthorRelationship(models.Model):
+    """
+    Through model that maps the many to many
+    relationship between Survey Home Page and Authors
+    """
+    author = models.ForeignKey(Person, on_delete=models.CASCADE, related_name="+")
+    page = ParentalKey('SurveysHomePage', related_name='authors')
+
+    panels = [
+        PageChooserPanel('author'),
+    ]
+    class Meta:
+        ordering = ['pk']
+
+
+class SurveysHomePage(AbstractContentPage):
     parent_page_types = ['programs.Project']
     subpage_types = ['Survey', 'Commentary', 'SurveyValuesIndex']
-
 
     partner_logo = models.ForeignKey(
         'home.CustomImage',
@@ -38,35 +46,54 @@ class ProgramSurveysPage(AbstractContentPage):
         on_delete=models.SET_NULL,
         related_name='+'
     )
+    about = RichTextField(
+      max_length=1500,
+      blank=True,
+      verbose_name='About This Project'
+    )
+    page_author = models.ManyToManyField(
+      Person,
+      through=PageAuthorRelationship,
+      blank=True
+    )
     subheading = models.CharField(max_length=200, blank=True)
-    about = RichTextField(max_length = 1500, blank=True, verbose_name='About This Project')
     methodology = RichTextField(max_length = 1500, blank=True)
-
 
     content_panels = [
       FieldPanel('title'),
       FieldPanel('subheading'),
       MultiFieldPanel([
-
         FieldPanel('about'),
         FieldPanel('methodology'),
+        InlinePanel('authors', label="Authors"),
         ImageChooserPanel('partner_logo'),
       ], heading='About Page')
     ]
 
+    search_fields = Page.search_fields + [
+      index.RelatedFields('page_author', [
+        index.SearchField('first_name'),
+        index.SearchField('last_name'),
+        index.SearchField('position_at_new_america'),
+      ]),
+      ]
+
+    def get_context(self, request):
+        context = super(SurveysHomePage, self).get_context(request)
+        context['authors'] = self.authors.order_by('pk')
+
+        return context
+
     def save(self, *args, **kwargs):
-      super(ProgramSurveysPage, self).save(*args, **kwargs)
+      super(SurveysHomePage, self).save(*args, **kwargs)
       if self.get_children_count() == 0:
         self.add_child(instance=SurveyValuesIndex(title=self.title + " Values Index", slug=slugify(self.title + " Values Index")))
-    
-    @property
-    def content_model(self):
-        return Survey
     class Meta:
         verbose_name = "Surveys Homepage"
 
     def __str__(self):
         return self.title
+
 
 class SurveyOrganization(Page):
     parent_page_types = ['SurveyValuesIndex']    
@@ -76,6 +103,7 @@ class SurveyOrganization(Page):
     class Meta:
         verbose_name_plural = 'Survey Organizations'
 
+
 class DemographicKey(Page):
     parent_page_types = ['SurveyValuesIndex']
     
@@ -83,6 +111,7 @@ class DemographicKey(Page):
       return self.title
     class Meta:
         verbose_name_plural = 'Demographic Keys'
+
 
 class SurveyTags(Page):
     parent_page_types = ['SurveyValuesIndex']
@@ -92,8 +121,9 @@ class SurveyTags(Page):
     class Meta:
         verbose_name_plural = 'Survey Tags'
 
+
 class SurveyValuesIndex(Page):
-    parent_page_types = ['ProgramSurveysPage']
+    parent_page_types = ['SurveysHomePage']
     subpage_type = ['SurveyOrganization', 'DemographicKey', 'SurveyTags']
     class Meta:
         verbose_name = "Surveyindex Homepage"
@@ -105,13 +135,13 @@ class SurveyValuesIndex(Page):
 class Survey(Post):
     template = 'survey/survey.html'
  
-    parent_page_types = ['ProgramSurveysPage']
+    parent_page_types = ['SurveysHomePage']
 
+    description = RichTextField(blank=True, null=True, max_length=500, help_text='A brief description of the survey. 500 chars max')
     org = ParentalManyToManyField('SurveyOrganization', related_name='SurveyOrganization', blank=True, verbose_name='Organization')
     year = models.IntegerField(help_text='Year Survey was condicted.', blank=True, default=2000)
     month = models.IntegerField(choices=MONTH_CHOICES, default=None, help_text='Month Survey was condicted, if applicable.')
     sample_number = models.IntegerField(blank=True, null=True)
-    sample_demos = models.CharField(max_length=250, blank=True, null=True, help_text='Text displayed on the dashboard', verbose_name='Sample Demographics')
     demos_key = ParentalManyToManyField('DemographicKey', help_text='Indexable demographic groups', blank=True, default=False, verbose_name='Demographics Keys')
     findings = RichTextField(blank=True, null=True, max_length=12500)
     data_type = MultiSelectField(choices=DATA_TYPE_CHOICES)
@@ -126,11 +156,11 @@ class Survey(Post):
         FieldPanel('date'),
       ], heading='Survey Created'),
       MultiFieldPanel([
+        FieldPanel('description'),
         AutocompletePanel('org'),
         FieldPanel('year'),
         FieldPanel('month'),
         FieldPanel('sample_number'),
-        FieldPanel('sample_demos'),
         AutocompletePanel('demos_key'),
         AutocompletePanel('tags'),
         FieldPanel('data_type', widget=forms.CheckboxSelectMultiple),
@@ -148,10 +178,10 @@ Survey._meta.get_field('title').help_text="Title of the Survey"
 Survey._meta.get_field('date').help_text="Date that the Survey was collected."
 
 
-class Commentary(Post, RoutablePageMixin):
+class Commentary(Post):
   template = 'survey/commentary.html'
 
-  parent_page_types = ['ProgramSurveysPage']
+  parent_page_types = ['SurveysHomePage']
 
   assoc_surveys = ParentalManyToManyField('Survey', through='Commented_Survey', blank=True, related_name='commentaries')
 
@@ -161,16 +191,16 @@ class Commentary(Post, RoutablePageMixin):
       FieldPanel('date'),
       StreamFieldPanel('body'),
       InlinePanel('authors', label=("Authors")),
-      InlinePanel('topics', label=("Topics")),
       AutocompletePanel('assoc_surveys')
   ]
 
   class Meta:
-      verbose_name = 'Expert Commentary'
+      verbose_name = 'Insights & Analysis'
 
   def __str__(self):
         return self.title
 
 class Commented_Survey(models.Model):
   survey=models.ForeignKey('Survey', on_delete=models.CASCADE, blank=True, null=True)
-  commentary=models.ForeignKey('Commentary', on_delete=models.CASCADE, blank=True, null=True)
+  commentary = models.ForeignKey('Commentary', on_delete=models.CASCADE, blank=True, null=True)
+  
