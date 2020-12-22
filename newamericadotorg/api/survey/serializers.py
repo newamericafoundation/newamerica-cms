5f2c54bb381f3f11b5fe7648816502a1428d4884
Daniel Sasser <dsasser@phase2technology.com>
2020-12-22 11:36:01 -0800

    Update API based on model changes.

--- newamericadotorg/api/survey/serializers.py
+++ newamericadotorg/api/survey/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
-from survey.models import Survey, ProgramSurveysPage, Commentary
+from survey.models import Survey, SurveysHomePage
 from programs.models import Subprogram
 
 class SurveyHomeSerializer(ModelSerializer):
@@ -7,44 +7,55 @@ class SurveyHomeSerializer(ModelSerializer):
 
     class Meta:
         model = Subprogram
-        fields = ('__all__')
+        fields = ['id', 'title', 'survey_home_page', 'seo_title', 'name', 'url_path']
 
     def get_survey_home_page(self, obj):
         return get_survey_homepage(obj)
 
-class ProgramSurveysPageSerializer(ModelSerializer):
+class SurveysHomePageSerializer(ModelSerializer):
     surveys = SerializerMethodField()
 
     class Meta:
-        model = ProgramSurveysPage
-        fields = '__all__'
-
+        model = SurveysHomePage
+        fields = ['id', 'title', 'url_path', 'about', 'methodology', 'page_author', 'surveys']
+        depth = 1
     def get_surveys(self, obj):
         return get_surveys_detail(obj)
 
 class SurveyDetailSerializer(ModelSerializer):
-    survey_commentary = SerializerMethodField()
 
     class Meta:
         model = Survey
-        depth = 2
-        fields = '__all__'
-
-    def get_survey_commentary(self, obj):
-        return get_survey_commentary_detail(obj)
-
-class CommentarySerializer(ModelSerializer):
-    class Meta:
-        model = Commentary
-        fields = '__all__'
+        depth = 1
+        fields = [
+            'id',
+            'title',
+            'slug',
+            'url_path',
+            'seo_title',
+            'subheading',
+            'description',
+            'year',
+            'month',
+            'sample_number',
+            'findings',
+            'data_type',
+            'national',
+            'link',
+            'file',
+            'org',
+            'demos_key',
+            'assoc_commentary',
+            'tags'
+        ]
 
 def get_survey_homepage(page):
-    homePage = page.get_children().type(ProgramSurveysPage).live().first()
+    homePage = page.get_children().type(SurveysHomePage).live().first()
     if homePage is not None:
-        surveyHomePage = ProgramSurveysPageSerializer(homePage).data
+        surveyHomePage = SurveysHomePageSerializer(homePage.specific).data
         return surveyHomePage
     else:
-        return None
+        return []
 
 def get_surveys_detail(homePage):
     children = homePage.get_children().type(Survey).live()
@@ -53,10 +64,3 @@ def get_surveys_detail(homePage):
         for c in children:
             surveys.append(SurveyDetailSerializer(c.specific).data)
     return surveys
-
-def get_survey_commentary_detail(survey):
-    commentary = CommentarySerializer(Commentary.objects.filter(assoc_surveys=survey.id).live().first().specific)
-    if commentary is not None:
-        return commentary.data
-    else:
-         return []
