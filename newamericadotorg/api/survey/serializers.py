4de45c4c348f35535b2dd8ce3ff13cccd1c485d6
IMGoodrich <igoodrich@phase2technology.com>
2021-01-04 12:58:47 -0800

    chore: Update survey serializers to include subprogram desctiption, and expose author and partner logo img src.

--- newamericadotorg/api/survey/serializers.py
+++ newamericadotorg/api/survey/serializers.py
@@ -1,26 +1,41 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from survey.models import Survey, SurveysHomePage
 from programs.models import Subprogram
+from newamericadotorg.api.author.serializers import AuthorSerializer
+from wagtail.images.views.serve import generate_image_url
 
 class SurveyHomeSerializer(ModelSerializer):
     survey_home_page = SerializerMethodField()
 
     class Meta:
         model = Subprogram
-        fields = ['id', 'title', 'survey_home_page', 'seo_title', 'name', 'url_path']
+        fields = ['id', 'title', 'survey_home_page', 'seo_title', 'name', 'url_path', 'description']
 
     def get_survey_home_page(self, obj):
         return get_survey_homepage(obj)
 
 class SurveysHomePageSerializer(ModelSerializer):
     surveys = SerializerMethodField()
+    page_author = SerializerMethodField()
+    partner_logo = SerializerMethodField()
 
     class Meta:
         model = SurveysHomePage
-        fields = ['id', 'title', 'url_path', 'about', 'methodology', 'page_author', 'surveys']
+        fields = ['id', 'title', 'url_path', 'about', 'methodology', 'page_author', 'surveys', 'partner_logo']
         depth = 1
     def get_surveys(self, obj):
         return get_surveys_detail(obj)
+    
+    def get_page_author(self, obj):
+        authors_rel = obj.authors.all()
+        if not authors_rel:
+            return None
+        authors = [rel.author for rel in authors_rel]
+        return AuthorSerializer(authors, many=True, context=self.context).data
+   
+    def get_partner_logo(self, obj):
+        if obj.partner_logo:
+            return generate_image_url(obj.partner_logo, 'max-240x30')
 
 class SurveyDetailSerializer(ModelSerializer):
 
