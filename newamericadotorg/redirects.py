b1c51b19f518cd47971e8a0a3498d7a7627ae622
nmorduch <1051450+nmorduch@users.noreply.github.com>
2021-01-21 12:59:28 -0500

    Add redirect for FLHP and update old redirect for them

--- newamericadotorg/redirects.py
+++ newamericadotorg/redirects.py
@@ -3,9 +3,10 @@ from django.shortcuts import render, redirect
 
 # Subprograms changing to programs
 
+# now FLHP
 def future_property_rights(request, **kwargs):
     path = request.path.split('/')[3:]
-    url = '/future-property-rights/' + '/'.join(path)
+    url = '/flhp/' + '/'.join(path)
     return redirect(url)
 
 def new_practice_lab(request, **kwargs):
@@ -37,5 +38,11 @@ def local(request, **kwargs):
 def pit(request, **kwargs):
     path = request.path.split('/')
     path[1] = 'pit'
+    url = '/'.join(path)
+    return redirect(url)
+
+def flhp(request, **kwargs):
+    path = request.path.split('/')
+    path[1] = 'flhp'
     url = '/'.join(path)
     return redirect(url)
