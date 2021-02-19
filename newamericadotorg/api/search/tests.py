1107162550aa338f9f3bd57342925842b43c9b10
Cameron Higby-Naquin <me@chigby.org>
2021-02-19 09:40:53 -0500

    Improve reliability on sometimes-flickering test
    I've seen this test fail sometimes on CI and I'm never sure exactly
    why, this is my attempt to get more information out of the assertion
    failure.
    
    What I think was happening here is the original test was not actually
    testing what we wanted it to be testing, namely that *all* the results
    are mostly in order.  We were just testing that the first result was
    the most recent (and this was not always the case).
    
    What I've done here is to move the test into its own class and remove
    some additional sources of randomness that were interfering with how
    ES was ordering the results.  Namely, I've given all the tests exactly
    the same text data, rather than random fake data.

--- newamericadotorg/api/search/tests.py
+++ newamericadotorg/api/search/tests.py
@@ -58,31 +58,6 @@ class SearchAPITests(APITestCase):
         self.assertNotIn('error', result)
         self.assertEquals(result['count'], 0)
 
-    def test_date_factored_into_ranking(self):
-        # Seed the random number generators so the generated numbers are deterministic
-        random.seed(a=123456, version=2)
-
-        for post in self.posts:
-            post.date = datetime.date.today() - datetime.timedelta(days=random.randint(0, 200) * 50)
-            post.save()
-
-        url = '/api/search/?query=unique query'
-        response = self.client.get(url).json()
-
-        self.assertNotIn('error', response)
-        self.assertEquals(response['count'], 50)
-
-        # Check that posts are returned in descending date order
-        # This is only factored into the regular ranking algorithm, but in
-        # this case all posts have the same title so the only ranking factor is
-        # the date
-        parse_date = lambda date_str: datetime.date(*map(int, date_str.split('-')))
-
-        previous_date = parse_date(response['results'][0]['date'])
-        for result in response['results'][1:]:
-            date = parse_date(result['date'])
-            self.assertTrue(date <= previous_date)
-
     def test_restricted(self):
         for p in self.posts[:25]:
             last_restriction = PageViewRestriction.objects.create(page=p, restriction_type=PageViewRestriction.PASSWORD)
@@ -103,6 +78,75 @@ class SearchAPITests(APITestCase):
 
         self.assertNotIn('error', result)
         self.assertEquals(result['count'], 26)
+
+
+
+@unittest.skipUnless(TEST_ELASTICSEARCH, "Elasticsearch tests not enabled")
+class SearchAPIDateTests2(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        home_page = PostFactory.create_homepage()
+
+        program = PostFactory.create_program(home_page=home_page)
+
+        blog_posts_page = PostFactory.create_program_content_page(
+            program=program,
+            content_page_type=ProgramBlogPostsPage,
+        )
+
+        cls.posts = []
+
+        for i in range(50):
+            post_data = {
+                'title': 'Unique Query',
+                'date': datetime.date.today() - datetime.timedelta(days=random.randint(0, 200) * 50),
+                'slug': f'unique-query-{i}',
+                'story_excerpt': 'Unique query'
+            }
+            cls.posts.extend(
+                PostFactory.create_content(
+                    1,
+                    content_page=blog_posts_page,
+                    post_type=BlogPost,
+                    post_data=post_data,
+                )
+            )
+
+        # Some posts that shouldn't be returned in results
+        PostFactory.create_program_content(10,
+            program=program,
+            content_page_type=ProgramBlogPostsPage,
+            post_type=BlogPost,
+            post_data={'title': 'Foo'}
+        )
+
+    def setUp(self):
+        management.call_command('update_index', stdout=StringIO(), chunk_size=50)
+
+    def test_date_factored_into_ranking(self):
+        dates = []
+        for post in self.posts:
+            dates.append(post.date)
+
+        url = '/api/search/?query=unique query'
+        response = self.client.get(url).json()
+
+        self.assertNotIn('error', response)
+        self.assertEquals(response['count'], 50)
+
+        # Check that posts are returned in descending date order
+        # This is only factored into the regular ranking algorithm, but in
+        # this case all posts have the same title so the only ranking factor is
+        # the date
+        parse_date = lambda date_str: datetime.date(*map(int, date_str.split('-')))
+
+        dates.sort(reverse=True)
+
+        self.maxDiff = None
+        self.assertEqual(
+            dates[:len(response['results'])],
+            [parse_date(result['date']) for result in response['results']]
+        )
 
 
 @unittest.skipUnless(TEST_ELASTICSEARCH, "Elasticsearch tests not enabled")
