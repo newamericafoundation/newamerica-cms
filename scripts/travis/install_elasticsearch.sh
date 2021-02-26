97eded1f716e38c662ef83ed827c583270b722e8
Cameron Higby-Naquin <me@chigby.org>
2021-02-26 15:05:03 -0500

    Update elasticsearch version on travis

--- scripts/travis/install_elasticsearch.sh
+++ scripts/travis/install_elasticsearch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -O https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.4.6/elasticsearch-2.4.6.deb
-dpkg -i --force-confnew elasticsearch-2.4.6.deb
+curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.16.deb
+dpkg -i --force-confnew elasticsearch-5.6.16.deb
 
 # Enable script scoring
 cat << EOF >> /etc/elasticsearch/elasticsearch.yml
