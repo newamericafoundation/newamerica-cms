ae1f2877e91e39e9984ad6fd80728b88d31ccea2
Cameron Higby-Naquin <me@chigby.org>
2021-04-29 14:55:12 -0400

    Limit who can use the "Publish" action for Brief pages

---
+++ brief/wagtail_hooks.py
@@ -0,0 +1,24 @@
+from wagtail.core import hooks
+
+from home.models import PublicationPermissions
+
+from .models import Brief
+
+
+@hooks.register('construct_page_action_menu')
+def remove_publish_brief_if_no_permission(menu_items, request, context):
+    """Remove the 'Publish' action from all briefs unless the user is a
+    superuser or in one of the auth groups specifically chosen for
+    this permission.
+
+    """
+    if isinstance(context.get('page'), Brief):
+        settings = PublicationPermissions.for_request(request)
+        if request.user.is_superuser:
+            # superusers can always publish
+            return
+
+        if not request.user.groups.all() & settings.groups_with_brief_permission.all():
+            menu_items[:] = [
+                item for item in menu_items if item.name != 'action-publish'
+            ]
