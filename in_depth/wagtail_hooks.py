6f7879045e746042daf89d3675f6dbf5fd101368
Cameron Higby-Naquin <me@chigby.org>
2021-05-21 09:58:50 -0400

    Apply report-permission limiting to InDepthProjects

---
+++ in_depth/wagtail_hooks.py
@@ -0,0 +1,28 @@
+from wagtail.core import hooks
+
+from home.models import PublicationPermissions
+
+from .models import InDepthProject, AllInDepthHomePage
+
+
+@hooks.register('construct_page_action_menu')
+def remove_publish_in_depth_if_no_permission(menu_items, request, context):
+    """Remove the 'Publish' action from all in depth projects unless the
+    user is a superuser or in one of the auth groups specifically
+    chosen for this permission.
+
+    """
+    editing_in_depth_page = isinstance(context.get('page'), InDepthProject)
+    creating_in_depth_page = context.get('view') == 'create' and isinstance(
+        context.get('parent_page'), AllInDepthHomePage
+    )
+    if editing_in_depth_page or creating_in_depth_page:
+        settings = PublicationPermissions.for_request(request)
+        if request.user.is_superuser:
+            # superusers can always publish
+            return
+
+        if not request.user.groups.all() & settings.groups_with_report_permission.all():
+            menu_items[:] = [
+                item for item in menu_items if item.name != 'action-publish'
+            ]
