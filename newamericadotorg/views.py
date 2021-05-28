1ecdbe25448d3bc92653a7d10ab7ae4cb0bb3321
Cameron Higby-Naquin <me@chigby.org>
2021-05-28 16:06:26 -0400

    Require login for preview view

--- newamericadotorg/views.py
+++ newamericadotorg/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+@login_required
 def preview(request, **kwargs):
     return render(request, 'preview.html', context={})
