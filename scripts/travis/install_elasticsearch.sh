e276a75af0b0e878c03d9ca5722aa1593a8d6f57
Cameron Higby-Naquin <me@chigby.org>
2021-09-23 11:04:11 -0400

    Update elasticsearch to 7.15.0

--- scripts/travis/install_elasticsearch.sh
+++ scripts/travis/install_elasticsearch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.13.4-amd64.deb
-dpkg -i --force-confnew elasticsearch-7.13.4-amd64.deb
+curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.15.0-amd64.deb
+dpkg -i --force-confnew elasticsearch-7.15.0-amd64.deb
 
 chown -R elasticsearch:elasticsearch /etc/default/elasticsearch
 
