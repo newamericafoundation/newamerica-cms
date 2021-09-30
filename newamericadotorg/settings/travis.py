2ac9bec772c3d435325d6b687e65a6fb23511d61
Cameron Higby-Naquin <me@chigby.org>
2021-09-30 10:39:54 -0400

    Add separate settings file for travis

---
+++ newamericadotorg/settings/travis.py
@@ -0,0 +1,31 @@
+from .base import *
+
+BASE_URL = "http://testserver"
+
+SECRET_KEY = "TEST_KEY"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'newamerica',
+        'HOST': 'localhost',
+    }
+}
+
+WAGTAILSEARCH_BACKENDS = {
+    'default': {
+        'BACKEND': 'search.backend',
+        'URLS': ["http://localhost:9200"],
+        'INDEX': 'test',
+        'TIMEOUT': 1500,
+        'INDEX_SETTINGS': {
+            'settings': {
+                'index': {
+                    'number_of_shards': 1,
+                },
+            },
+        }
+    }
+}
+
+TEST_ELASTICSEARCH = True
