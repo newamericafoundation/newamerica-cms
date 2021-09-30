ab91f652e17bffa5e219bb6583bba0304ef4cbef
Cameron Higby-Naquin <me@chigby.org>
2021-09-30 11:48:37 -0400

    Remove legacy scripts (#1751)
    Fixes #1739

--- home/management/commands/deletesite.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from wagtail.core.models import Page, Site
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        Site.objects.all().delete()
-        Page.objects.all().delete()
