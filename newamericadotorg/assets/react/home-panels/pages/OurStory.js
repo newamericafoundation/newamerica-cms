7540c8403c92c6add09b4f33304ed77922309e5c
Naomi I. Morduch Toubman <nmorduch@gmail.com>
2023-07-21 10:55:41 -0400

    Remove our vision section (#1794)

--- newamericadotorg/assets/react/home-panels/pages/OurStory.js
+++ newamericadotorg/assets/react/home-panels/pages/OurStory.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ImageAside from '../components/ImageAside';
-import Reel from '../components/Reel';
+// import Reel from '../components/Reel';
 import Body from '../components/Body';
 
 
@@ -11,7 +11,7 @@ export default class OurStory extends Component {
     return (
       <div className="home__panels__content">
         <ImageAside data={data.our_community} grayscale={true}/>
-        <Reel data={data.our_vision} interval={3000} />
+        {/* <Reel data={data.our_vision} interval={3000} /> */}
         <Body data={data.our_work} />
       </div>
     );
