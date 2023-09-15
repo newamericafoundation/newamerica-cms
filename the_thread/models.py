83f463d509d87b443fcb022eec927656f2ec2a16
Cameron Higby-Naquin <me@chigby.org>
2023-09-15 11:25:24 -0400

    Reformat for pep8 compliance and clean up imports

--- the_thread/models.py
+++ the_thread/models.py
@@ -1,50 +1,42 @@
-from __future__ import unicode_literals
-import json
-
 from django.db import models
-
-from home.models import Post
-
 from wagtail.admin.panels import FieldPanel
 from wagtail.models import Page
-from wagtail.fields import StreamField
-from wagtail.blocks import PageChooserBlock
 
+from home.models import AbstractHomeContentPage, Post
 from programs.models import AbstractContentPage
-from newamericadotorg.helpers import paginate_results
-from home.models import AbstractHomeContentPage
+
 
 class Thread(AbstractContentPage):
-    parent_page_types = ['home.HomePage']
-    subpage_types = ['ThreadArticle']
+    parent_page_types = ["home.HomePage"]
+    subpage_types = ["ThreadArticle"]
 
     featured_page_1 = models.ForeignKey(
-        'home.Post',
+        "home.Post",
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        related_name='+',
+        related_name="+",
     )
     featured_page_2 = models.ForeignKey(
-        'home.Post',
+        "home.Post",
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        related_name='+',
+        related_name="+",
     )
     featured_page_3 = models.ForeignKey(
-        'home.Post',
+        "home.Post",
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        related_name='+',
+        related_name="+",
     )
 
     content_panels = AbstractContentPage.content_panels + [
-        FieldPanel('featured_page_1'),
-        FieldPanel('featured_page_2'),
-        FieldPanel('featured_page_3'),
-    ] # x
+        FieldPanel("featured_page_1"),
+        FieldPanel("featured_page_2"),
+        FieldPanel("featured_page_3"),
+    ]  # x
 
     class Meta:
         verbose_name = "The Thread"
@@ -56,19 +48,19 @@ class ThreadEdition(Page):
 
 
 class ThreadArticle(Post):
-    parent_page_types = ['Thread']
+    parent_page_types = ["Thread"]
     subpage_types = []
 
     class Meta:
-        verbose_name = 'Article in The Thread'
+        verbose_name = "Article in The Thread"
 
 
 class AllThreadArticlesHomePage(AbstractHomeContentPage):
-    parent_page_types = ['home.HomePage']
+    parent_page_types = ["home.HomePage"]
     subpage_types = []
 
     class Meta:
-        verbose_name = 'Thread Articles Homepage'
+        verbose_name = "Thread Articles Homepage"
 
     @property
     def content_model(self):
