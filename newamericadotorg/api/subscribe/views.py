cf9bb99478d50959b9ffa9af3820e76bef823167
Cameron Higby-Naquin <me@chigby.org>
2023-09-29 16:54:14 -0400

    PEP8 and reformat file

--- newamericadotorg/api/subscribe/views.py
+++ newamericadotorg/api/subscribe/views.py
@@ -1,50 +1,49 @@
 import json
 import os
 from urllib.request import urlopen
-from rest_framework.response import Response
+
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from subscribe.campaign_monitor import update_subscriber
 
-@api_view(['POST'])
-@permission_classes((AllowAny, ))
+
+@api_view(["POST"])
+@permission_classes((AllowAny,))
 def subscribe(request):
     params = request.query_params
-    recaptcha_response = params.get('g-recaptcha-response', None)
+    recaptcha_response = params.get("g-recaptcha-response", None)
     if not recaptcha_response:
-        return Response({
-            'status': 'UNVERIFIED'
-        });
+        return Response({"status": "UNVERIFIED"})
 
-    RECAPTCHA_SECRET_KEY = os.getenv('RECAPTCHA_SECRET_KEY')
-    recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify?response=%s&secret=%s' % (recaptcha_response, RECAPTCHA_SECRET_KEY)
+    RECAPTCHA_SECRET_KEY = os.getenv("RECAPTCHA_SECRET_KEY")
+    recaptcha_url = (
+        "https://www.google.com/recaptcha/api/siteverify?response=%s&secret=%s"
+        % (recaptcha_response, RECAPTCHA_SECRET_KEY)
+    )
     verification = urlopen(recaptcha_url).read()
     verification = json.loads(verification)
 
-    if not verification['success']:
-        return Response({
-            'status': 'UNVERIFIED'
-        });
+    if not verification["success"]:
+        return Response({"status": "UNVERIFIED"})
 
-    subscriptions = params.getlist('subscriptions[]', None)
-    job_title = params.get('job_title', None)
-    org = params.get('organization', None)
-    zipcode = params.get('zipcode', None)
+    subscriptions = params.getlist("subscriptions[]", None)
+    job_title = params.get("job_title", None)
+    org = params.get("organization", None)
+    zipcode = params.get("zipcode", None)
     custom_fields = []
 
     if job_title:
-        custom_fields.append({ 'key': 'JobTitle', 'value': job_title })
+        custom_fields.append({"key": "JobTitle", "value": job_title})
     if org:
-        custom_fields.append({ 'key': 'Organization', 'value': org })
+        custom_fields.append({"key": "Organization", "value": org})
     if zipcode:
-        custom_fields.append({ 'key': 'MailingZip/PostalCode', 'value': zipcode })
+        custom_fields.append({"key": "MailingZip/PostalCode", "value": zipcode})
     if subscriptions:
         for s in subscriptions:
-            custom_fields.append({ 'key': 'Subscriptions', 'value': s })
+            custom_fields.append({"key": "Subscriptions", "value": s})
 
-    status = update_subscriber(params.get('email'), params.get('name'), custom_fields)
+    status = update_subscriber(params.get("email"), params.get("name"), custom_fields)
 
-    return Response({
-        'status': status
-    });
+    return Response({"status": status})
