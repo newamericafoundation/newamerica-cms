c407036fa66d7b658d40d68f2d07bbe883140cff
Cameron Higby-Naquin <me@chigby.org>
2024-05-31 17:09:54 -0400

    Add RDS environment variables to configuration

--- newamericadotorg/settings/eb_production.py
+++ newamericadotorg/settings/eb_production.py
@@ -2,6 +2,18 @@ import os
 
 from .base import *  # noqa: F403
 
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+
 # Timezone settings
 TIME_ZONE = 'America/New_York'
 USE_TZ = True
